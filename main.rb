2daa89a8d436a52de8fa9ec8d26ab158cdbc18e0
Fukayanegi <fukayanegi1981@gmail.com>
2015-12-08 14:51:38 +0900

    implement foundation #1

--- main.rb
+++ main.rb
@@ -1,1 +1,19 @@
-puts ARGV
+def solve lor
+  n = lor.to_i
+  return { b:0, c:0 } if n <= 0
+  return { b:1, c:1 } if n == 1
+
+  if n > 1 then
+    return { b:1, c:1 }
+  end
+end
+
+limit_of_return = ARGV[0]
+puts "INPUT : P = " + limit_of_return
+
+answer_tmp = solve limit_of_return
+answer = answer_tmp.values.inject(:+)
+
+puts "ANSWER : N = " + answer.to_s
+
+answer
