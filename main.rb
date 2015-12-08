8aac66113871586269399eb1be476469120c9669
Fukayanegi <fukayanegi1981@gmail.com>
2015-12-08 20:16:23 +0900

    implement solution for N of 1 to 5 #1

--- main.rb
+++ main.rb
@@ -1,19 +1,24 @@
-def solve lor
+def solve lor, status
   n = lor.to_i
-  return { b:0, c:0 } if n <= 0
-  return { b:1, c:1 } if n == 1
+  return { b: 0, c: 0, x: 0 } if n <= 0
+  return { b: 1, c: 1, x: 0 } if n <= 2
 
-  if n > 1 then
-    return { b:1, c:1 }
+  if n > 2 then
+    n = n - 1 if n.even?
+    answer_tmp = solve n - 2, status
+    new_b = answer_tmp[:b] * 1 + answer_tmp[:c] * 1 
+    new_c = answer_tmp[:b] * 1 + answer_tmp[:c] * 2
+    return { b: new_b, c: new_c, x: answer_tmp[:b] + answer_tmp[:c] + answer_tmp[:x] }
   end
 end
 
 limit_of_return = ARGV[0]
-puts "INPUT : P = " + limit_of_return
+initial = { b:0, c:0 }
+puts "INPUT : N = " + limit_of_return
 
-answer_tmp = solve limit_of_return
+answer_tmp = solve limit_of_return, initial
 answer = answer_tmp.values.inject(:+)
 
-puts "ANSWER : N = " + answer.to_s
+puts "OUTPUT : P = " + answer.to_s
 
 answer
