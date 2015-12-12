4093a93be9e9b17ef678f20755d5e9bed9d511c0
Fukayanegi <fukayanegi1981@gmail.com>
2015-12-13 01:44:58 +0900

    implement code of recievement of input #3

--- tile_of_start_menu.rb
+++ tile_of_start_menu.rb
@@ -2,15 +2,24 @@ def solve
   [{A:0, B:0, C:0, D:0}]
 end
 
-n = STDIN.gets
-if n.nil?
-#  puts "Warning N must not be nil"
+size = STDIN.gets
+if size.nil?
+  puts "Warning size must not be nil"
   exit 0
+end
+
+width, height = size.split(',')
+puts "INPUT : Width = #{width}, Height = #{height}"
+begin
+  width, height = Integer(width), Integer(height)
+rescue
+  puts "Warning : Width or Height is not a number"
+  width, height = 0, 0
 end
 
 answer_tmp = solve
 answer = answer_tmp.count
 
-#puts "OUTPUT : P = " + answer.to_s
+puts "OUTPUT : Patterns = " + answer.to_s
 
 puts answer
