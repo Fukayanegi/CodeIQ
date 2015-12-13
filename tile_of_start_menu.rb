ad6002019e1c3c281a92578be6f7751aebce020c
Fukayanegi <fukayanegi1981@gmail.com>
2015-12-13 22:24:52 +0900

    implement code of availability of input of '4,2' #3

--- tile_of_start_menu.rb
+++ tile_of_start_menu.rb
@@ -34,7 +34,9 @@ end
 def solve_tile_pattern pattern_map, usages
   solution = []
   usages.each do |tiles|
-    solution << (solve_tile_pattern_by_usage pattern_map, tiles, [])
+    temp = (solve_tile_pattern_by_usage Marshal.load(Marshal.dump(pattern_map)), tiles, [])
+    p temp
+    solution.concat temp
   end
   return solution
 end
@@ -46,17 +48,17 @@ def solve_tile_pattern_by_usage pattern_map, tiles, solution
   pattern_map.each_with_index do |row, i|
     row.each_with_index do |column, j|
       if put? pattern_map, i, j, tile_matrix then
+        pattern_map_dup = Marshal.load(Marshal.dump(pattern_map))
         tile_matrix.each_with_index do |row, k|
           row.each_with_index do |col, l|
-            pattern_map[i+k][j+l] = tile
+            pattern_map_dup[i+k][j+l] = tile
           end
         end
 
         if tiles.length == 0 then
-          solution << pattern_map
+          solution << pattern_map_dup
         else
-          ret = solve_tile_pattern_by_usage pattern_map.dup, tiles.dup, solution
-          solution << ret unless ret.nil?
+          solve_tile_pattern_by_usage pattern_map_dup, tiles.dup, solution
         end
       else
         nil
@@ -70,6 +72,7 @@ end
 def put? pattern_map, x, y, tile
   tile.each_with_index do |row, i|
     row.each_with_index do |column, j|
+      return false if (pattern_map.length <= x + i || pattern_map[0].length <= y + j)
       return false if pattern_map[x+i][y+j] != 0
     end
   end
@@ -94,7 +97,7 @@ end
 answer_tmp = solve width, height
 answer = answer_tmp.count
 
-p answer_tmp
+#p answer_tmp
 puts "OUTPUT : Patterns = " + answer.to_s
 
 puts answer
