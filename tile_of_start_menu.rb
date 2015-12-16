d0e00a3d4eef19c2748406df038f05d3493c69f9
Fukayanegi <fukayanegi1981@gmail.com>
2015-12-16 22:12:12 +0900

    recactoring but slow #3

--- tile_of_start_menu.rb
+++ tile_of_start_menu.rb
@@ -1,4 +1,5 @@
 SIZES = {A:1, B:4, C:8, D:16}
+@@NEST = 0
 A = [[1]]
 B = [[1, 1], [1, 1]]
 C = [[1, 1, 1, 1], [1, 1, 1, 1]]
@@ -6,9 +7,8 @@ D = [[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]]
 
 def solve width, height
   usages = solve_tile_usage width * height, [], [:D, :C, :B, :A]
-  p usages.uniq
-  #patterns = solve_tile_pattern Array.new(height) {Array.new(width, 0)}, usages.uniq
-  #patterns.uniq
+  patterns = solve_tile_pattern Array.new(height) {Array.new(width, 0)}, usages.uniq
+  patterns.uniq
 end
 
 def solve_tile_usage size, usage, choice
@@ -37,13 +37,16 @@ end
 def solve_tile_pattern pattern_map, usages
   solution = []
   usages.each do |tiles|
-    temp = (solve_tile_pattern_by_usage Marshal.load(Marshal.dump(pattern_map)), tiles, [])
+    temp = (solve_tile_pattern_by_usage Marshal.load(Marshal.dump(pattern_map)), tiles)
     solution.concat temp
   end
   return solution
 end
 
-def solve_tile_pattern_by_usage pattern_map, tiles, solution
+def solve_tile_pattern_by_usage pattern_map, tiles
+  solution = []
+  p tiles
+
   if tiles.uniq.count == 1 && tiles[0] == :A then
     pattern_map.each_with_index do |row, i|
       row.each_with_index do |column, j|
@@ -51,16 +54,40 @@ def solve_tile_pattern_by_usage pattern_map, tiles, solution
       end
     end
     solution << pattern_map
+  elsif tiles.uniq.count == 1
+    tiles.each do |tile|
+      tile_matrix = eval(tile.to_s)
+      pattern_map.each_with_index do |row, i|
+        row.each_with_index do |cpl, j|
+          if put? pattern_map, i, j, tile_matrix then
+            tile_matrix.each_with_index do |t_row, k|
+              t_row.each_with_index do |t_col, l|
+                pattern_map[i+k][j+l] = tile
+              end
+            end
+          end
+        end
+      end
+    end
+
+    invalid = pattern_map.inject(0) do |sum, row| 
+      sum += row.inject(0) do |sum, col|
+        sum += 1 if col.class != Symbol
+        sum
+      end
+    end
+    solution << pattern_map if invalid == 0
   else
     tile = tiles.shift
     tile_matrix = eval(tile.to_s)
 
     pattern_map.each_with_index do |row, i|
-      row.each_with_index do |column, j|
+      row.each_with_index do |cpl, j|
         if put? pattern_map, i, j, tile_matrix then
           pattern_map_dup = Marshal.load(Marshal.dump(pattern_map))
-          tile_matrix.each_with_index do |row, k|
-            row.each_with_index do |col, l|
+
+          tile_matrix.each_with_index do |t_row, k|
+            t_row.each_with_index do |t_col, l|
               pattern_map_dup[i+k][j+l] = tile
             end
           end
@@ -68,10 +95,9 @@ def solve_tile_pattern_by_usage pattern_map, tiles, solution
           if tiles.length == 0 then
             solution << pattern_map_dup
           else
-            solve_tile_pattern_by_usage pattern_map_dup, tiles.dup, solution
+            ret = solve_tile_pattern_by_usage pattern_map_dup, tiles.dup
+            solution << ret if ret != []
           end
-        else
-          nil
         end
       end
     end
@@ -108,7 +134,7 @@ end
 answer_tmp = solve width, height
 answer = answer_tmp.count
 
-# p answer_tmp
+#p answer_tmp
 #puts "OUTPUT : Patterns = " + answer.to_s
 
 puts answer
