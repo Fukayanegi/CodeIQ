4bc0d47cff62e0508a8e6689016b9daae364bec1
Fukayanegi <fukayanegi1981@gmail.com>
2015-12-17 00:08:57 +0900

    implement solve_implove #3

--- tile_of_start_menu.rb
+++ tile_of_start_menu.rb
@@ -5,9 +5,64 @@ C = [[1, 1, 1, 1], [1, 1, 1, 1]]
 D = [[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]]
 
 def solve width, height
-  usages = solve_tile_usage width * height, [], [:D, :C, :B, :A]
-  patterns = solve_tile_pattern Array.new(height) {Array.new(width, 0)}, usages.uniq
+  patterns = solve_improve Array.new(height) {Array.new(width, 0)}, [:D, :C, :B, :A]
+  p patterns.uniq
   patterns.uniq
+  # usages = solve_tile_usage width * height, [], [:D, :C, :B, :A]
+  # patterns = solve_tile_pattern Array.new(height) {Array.new(width, 0)}, usages.uniq
+  # patterns.uniq
+end
+
+def solve_improve pattern_map, choices
+  solution = []
+  first_choice = choices[0]
+  tile = eval(first_choice.to_s)
+  p first_choice
+
+  if first_choice == :A then
+    pattern_map.each_with_index do |row, i|
+      row.each_with_index do |column, j|
+        pattern_map[i][j] = :A
+      end
+    end
+    solution << pattern_map
+  elsif put? pattern_map, 0, 0, tile
+    pattern_map_dup = Marshal.load(Marshal.dump(pattern_map))
+    put pattern_map_dup, 0, 0, tile, first_choice
+
+    if tile.length == pattern_map_dup.length && 
+      tile[0].length == pattern_map_dup[0].length
+      solution << pattern_map_dup
+    else
+      divide_map = divide pattern_map_dup, tile[0].length, tile.length
+      solution_temp1 = solve_improve divide_map, choices.dup
+      solution_temp2 = solve_improve pattern_map_dup, choices.dup
+      solution_temp1.each do |matrix_1|
+        solution_temp2.each do |matrix_2|
+          solution << (join matrix_1, matrix_2)
+        end
+      end
+      p solution
+
+      pattern_map_dup_2 = Marshal.load(Marshal.dump(pattern_map))
+      divide_map_2 = divide pattern_map_dup_2, 1, tile[0].length
+      solution_temp1_2 = solve_improve divide_map_2, choices.dup
+      solution_temp2_2 = solve_improve pattern_map_dup_2, choices.dup
+      p solution_temp1_2 
+      p solution_temp2_2 
+      solution_temp1_2.each do |matrix_1|
+        solution_temp2_2.each do |matrix_2|
+          solution << (join matrix_1, matrix_2)
+        end
+      end
+    end
+  end
+
+  choices.shift
+  if choices.length > 0 then
+    solution.concat (solve_improve pattern_map, choices)
+  end
+  return solution
 end
 
 def solve_tile_usage size, usage, choice
@@ -113,6 +168,40 @@ def put? pattern_map, x, y, tile
     end
   end
   return true
+end
+
+def put pattern_map, x, y, tile, sym
+  tile.each_with_index do |row, i|
+    row.each_with_index do |column, j|
+      pattern_map[x+i][y+j] = sym
+    end
+  end
+end
+
+def divide pattern_map, width, height
+  splited_map = Array.new(height) {Array.new(width, 0)}
+  height.times do |i|
+    width.times do |j|
+      pattern_map[i].shift
+    end
+  end
+  splited_map
+end
+
+def join pattern_map, tile
+  pattern_map_dup = Marshal.load(Marshal.dump(pattern_map))
+  tile.each_with_index do |row, i|
+    row.each do |column|
+      pattern_map_dup[i] << column
+    end
+  end
+  pattern_map_dup
+end
+
+def union pattern_map, tile
+  tile.each do |row|
+    pattern_map << row
+  end
 end
 
 size = STDIN.gets
