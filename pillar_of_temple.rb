b977cb38b6ce8f5e85aa02922f1b6886b099b05c
Fukayanegi <fukayanegi1981@gmail.com>
2015-12-25 22:52:46 +0900

    fix logic if true #5

--- pillar_of_temple.rb
+++ pillar_of_temple.rb
@@ -1,7 +1,7 @@
 require 'set'
 
 # @start = "12345678900"
-# @fin = "02345678911"
+# @fin = "02345678910"
 @start = "1234500"
 @fin = "0234510"
 @ptn = Set.new
@@ -34,8 +34,8 @@ def solve start, fin
   @mid_b = @mid_u  + @p_len / 2
   @half = @p_len / 2
 
-  p 'before'
-  p_placements start
+  # p 'before'
+  # p_placements start
   # p_placements fin
   before_placements_from_s = start
   before_placements_from_f = fin
@@ -48,10 +48,14 @@ def solve start, fin
       next_placements_from_s.concat (move_all placement)
     end
 
-    next_placements_from_s.each do |placement|
-      found = before_placements_from_f.any? do |p|
+    target_s = next_placements_from_s.select{|plc| plc[@mid_u] == "0" || plc[@mid_b] == "0"}
+
+    target_s.each do |placement1|
+      found = found || before_placements_from_f.any? do |placement2|
         # p "#{placement[0..@p_len -1]} #{p[0..@p_len -1]}" if count == 9
-        placement[0..(@p_len -1)] == p[0..(@p_len -1)]
+        p_pillar "#{placement1}" if placement1[0..(@p_len -1)] == placement2[0..(@p_len -1)]
+        p_pillar "#{placement2}" if placement1[0..(@p_len -1)] == placement2[0..(@p_len -1)]
+          placement1[0..(@p_len -1)] == placement2[0..(@p_len -1)]
       end
     end
 
@@ -65,27 +69,27 @@ def solve start, fin
     before_placements_from_s = next_placements_from_s
     before_placements_from_f = next_placements_from_f
 
-    p "#{count}times after"
-    p_placements next_placements_from_s
+    # p "#{count}times after"
+    # p_placements next_placements_from_s
     # p_placements next_placements_from_f
 
-    # target_s = before_placements_from_s.select{|plc| plc[@mid_u] == "0" || plc[@mid_b] == "0"}
-    # target_f = before_placements_from_f.select{|plc| plc[@mid_u] == "0" || plc[@mid_b] == "0"}
+    target_s = before_placements_from_s.select{|plc| plc[@mid_u] == "0" || plc[@mid_b] == "0"}
+    target_f = before_placements_from_f.select{|plc| plc[@mid_u] == "0" || plc[@mid_b] == "0"}
     target_s = before_placements_from_s
     target_f = before_placements_from_f
 
     # p_placements target_s
     # p_placements target_f
 
-    target_s.each do |placement|
-      found = target_f.any? do |p|
+    target_s.each do |placement1|
+      found = found || target_f.any? do |placement2|
         # p "s #{placement[0..9]}"
         # p "f #{p[0..9]}"
         # placement[0..9] == p[0..9]
-        placement[0..(@p_len -1)] == p[0..(@p_len -1)]
+        placement1[0..(@p_len -1)] == placement2[0..(@p_len -1)]
       end
     end
-    # p count
+    p count
     # found = true if count == 3
     # p_placements next_placements_from_s if count == 1
     # p_placements @ptn if count == 1
