22dfa6df936d1180a771f7b4f3ccf93e454f18d9
Fukayanegi <fukayanegi1981@gmail.com>
2015-12-25 22:32:28 +0900

    implement code which is able to solve 6 pillars #5

--- pillar_of_temple.rb
+++ pillar_of_temple.rb
@@ -1,10 +1,15 @@
-@start = "12345678900"
-@fin = "02345678911"
+require 'set'
+
+# @start = "12345678900"
+# @fin = "02345678911"
+@start = "1234500"
+@fin = "0234510"
+@ptn = Set.new
 
 def p_pillar placement
   p "*"*30
-  p placement[0..4]
-  p placement[5..9]
+  p placement[0..(@p_len / 2 - 1)]
+  p placement[3..(@p_len)]
 end
 
 def p_placements placements
@@ -15,7 +20,7 @@ end
 
 def p_placements2 placements
   placements.each do |placement|
-    p placement
+    p placement[0..9]
   end
 end
 
@@ -24,20 +29,33 @@ def solve start, fin
   before_placements_from_f = []
   found = false
   count = 0
+  @p_len = start[0].length - 1
+  @mid_u = (@p_len / 2) / 2
+  @mid_b = @mid_u  + @p_len / 2
+  @half = @p_len / 2
 
-  # p 'before'
+  p 'before'
+  p_placements start
   # p_placements fin
   before_placements_from_s = start
   before_placements_from_f = fin
 
   while !found do
-  # count.times do |i|
     next_placements_from_s = []
     next_placements_from_f = []
 
     before_placements_from_s.each do |placement|
       next_placements_from_s.concat (move_all placement)
     end
+
+    next_placements_from_s.each do |placement|
+      found = before_placements_from_f.any? do |p|
+        # p "#{placement[0..@p_len -1]} #{p[0..@p_len -1]}" if count == 9
+        placement[0..(@p_len -1)] == p[0..(@p_len -1)]
+      end
+    end
+
+    break if found
 
     before_placements_from_f.each do |placement|
       next_placements_from_f.concat (move_all placement)
@@ -47,19 +65,31 @@ def solve start, fin
     before_placements_from_s = next_placements_from_s
     before_placements_from_f = next_placements_from_f
 
-    # p "#{count+1}times after"
+    p "#{count}times after"
+    p_placements next_placements_from_s
     # p_placements next_placements_from_f
 
-    next_placements_from_s.each do |placement|
-      found = next_placements_from_f.any? do |p|
-        p "s #{placement[0..9]}"
-        p "f #{p[0..9]}"
-        placement[0..9] == p[0..9]
+    # target_s = before_placements_from_s.select{|plc| plc[@mid_u] == "0" || plc[@mid_b] == "0"}
+    # target_f = before_placements_from_f.select{|plc| plc[@mid_u] == "0" || plc[@mid_b] == "0"}
+    target_s = before_placements_from_s
+    target_f = before_placements_from_f
+
+    # p_placements target_s
+    # p_placements target_f
+
+    target_s.each do |placement|
+      found = target_f.any? do |p|
+        # p "s #{placement[0..9]}"
+        # p "f #{p[0..9]}"
+        # placement[0..9] == p[0..9]
+        placement[0..(@p_len -1)] == p[0..(@p_len -1)]
       end
     end
-    p count
-    found = true if count == 3
-    # p_placements2 next_placements_from_s if count == 10
+    # p count
+    # found = true if count == 3
+    # p_placements next_placements_from_s if count == 1
+    # p_placements @ptn if count == 1
+    # p "#{@ptn.length}"
   end
 
   return found ? count : -1*before_placements_from_s.length
@@ -69,7 +99,7 @@ def move placement, before, after
   p_dup = placement.dup
   tmp = p_dup[after]
   p_dup[after], p_dup[before] = p_dup[before], tmp
-  p_dup[10] = before.to_s
+  p_dup[@p_len] = before.to_s
 
   p_dup
 end
@@ -77,26 +107,34 @@ end
 def move_all placement
   placements = []
   now = placement.index("0")
-  before = placement[10].to_i
-  if now < 5
-    if before != now + 5
-      placements << (move placement, now, now + 5)
+  before = placement[@p_len].to_i
+  # p "#{now}, #{@half}, #{before}"
+  # p "#{now - @half - 1}"
+  if now < @half
+    if before != now + @half
+      tmp = move placement, now, now + @half
+      placements << tmp if @ptn.add? tmp
     end
-    if before != now + 6 && now < 4
-      placements << (move placement, now, now + 6)
+    if before != now + @half + 1 && now < @half - 1
+      tmp = move placement, now, now + @half + 1
+      placements << tmp if @ptn.add? tmp
     end
-    if before != now + 4 && now > 0
-      placements << (move placement, now, now + 4)
+    if before != now + @half - 1 && now > 0
+      tmp = move placement, now, now + @half - 1
+      placements << tmp if @ptn.add? tmp
     end
   else
-    if before != now - 5
-      placements << (move placement, now, now - 5)
+    if before != now - @half
+      tmp = move placement, now, now - @half
+      placements << tmp if @ptn.add? tmp
     end
-    if before != now - 6 && now > 5
-      placements << (move placement, now, now - 6)
+    if before != now - @half - 1 && now > @half
+      tmp = move placement, now, now - @half - 1
+      placements << tmp if @ptn.add? tmp
     end
-    if before != now - 4 && now < 9
-      placements << (move placement, now, now - 4)
+    if before != now - @half + 1 && now < @p_len -1
+      tmp = move placement, now, now - @half + 1
+      placements << tmp if @ptn.add? tmp
     end
   end
 
