a3c5ea0bb234fdae0931b6252ff213e6ad03b94d
Fukayanegi <fukayanegi1981@gmail.com>
2015-12-26 22:33:44 +0900

    implement code which is able to solve sample imput #7

--- calendar_of_galaxy.rb
+++ calendar_of_galaxy.rb
@@ -1,4 +1,5 @@
 w = %w(t u v w x y z)
 ajst = [0,0,0,1,1,2,2,3,3,3,4]
 y,m,d = STDIN.gets.chomp.split(".")
-p (y.to_i-500)*345+(m.ord-65)*31+ajst[m.ord-65]+d.to_i
+# p (y.to_i-500)*345+(m.ord-65)*31+ajst[m.ord-65]+d.to_i
+p w[((y.to_i-500)*345+(m.ord-65)*31+ajst[m.ord-65]+d.to_i-1)%7]
