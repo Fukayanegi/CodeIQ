b79115e6a1299d2c7c9f1f61fb715eec39ce88b1
Fukayanegi <fukayanegi1981@gmail.com>
2015-12-26 09:28:46 +0900

    implement code calculating days from origin #7

--- calendar_of_galaxy.rb
+++ calendar_of_galaxy.rb
@@ -1,3 +1,4 @@
-y = %w(t u v w x y z)
-i = STDIN.gets.chomp.split(".")
-p i
+w = %w(t u v w x y z)
+ajst = [0,0,0,1,1,2,2,3,3,3,4]
+y,m,d = STDIN.gets.chomp.split(".")
+p (y.to_i-500)*345+(m.ord-65)*31+ajst[m.ord-65]+d.to_i
