ea4b14c326d0c3c905c6d0c1e355fd239cc0595f
Fukayanegi <fukayanegi1981@gmail.com>
2015-12-26 23:00:40 +0900

    minimize but can't commpress under 71 bytes #7

--- calendar_of_galaxy.rb
+++ calendar_of_galaxy.rb
@@ -1,5 +1,3 @@
-w = %w(t u v w x y z)
-ajst = [0,0,0,1,1,2,2,3,3,3,4]
-y,m,d = STDIN.gets.chomp.split(".")
-# p (y.to_i-500)*345+(m.ord-65)*31+ajst[m.ord-65]+d.to_i
-p w[((y.to_i-500)*345+(m.ord-65)*31+ajst[m.ord-65]+d.to_i-1)%7]
+a=[0,31,62,94,125,157,188,220,251,282,314]
+y,m,d=STDIN.gets.chomp.split(".")
+puts (116+((y.to_i-500)*345+a[(m.ord-65)]+d.to_i-1)%7).chr
