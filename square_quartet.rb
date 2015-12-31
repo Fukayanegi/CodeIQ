e933e9ecadfb840a9e87ae338cfca454dd1a4ab4
Fukayanegi <fukayanegi1981@gmail.com>
2015-12-31 23:21:25 +0900

    simplify expression #14

--- square_quartet.rb
+++ square_quartet.rb
@@ -8,10 +8,10 @@ p "#{fin}"
 x = 2
 
 while x <= fin do 
-  if 4*x**2-4*diff > 0
-    d = Math.sqrt(4*x**2-4*diff)
+  if x**2-diff > 0
+    d = Math.sqrt(x**2-diff)
     if d % 1 == 0
-      sol1, sol2 = (2*x+d)/2, (2*x-d)/2
+      sol1, sol2 = x+d, x-d
       if x-sol1 > 0
         answer << x.to_i
         answer << (x-sol1).to_i
@@ -20,6 +20,10 @@ while x <= fin do
         answer << x
         answer << (x-sol2).to_i
       end
+    else
+      # while !(((x+1)**2-diff) % 1 == 0) do
+      #   x += 1
+      # end
     end
     # p "#{x}, #{sol1}, #{sol2}"
   end
