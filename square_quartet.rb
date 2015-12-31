b453f81eadd7718f80954c6c208be05c10029c61
Fukayanegi <fukayanegi1981@gmail.com>
2015-12-31 22:54:11 +0900

    put out if-statement of judgement of integer #14

--- square_quartet.rb
+++ square_quartet.rb
@@ -10,14 +10,16 @@ x = 2
 while x <= fin do 
   if 4*x**2-4*diff > 0
     d = Math.sqrt(4*x**2-4*diff)
-    sol1, sol2 = (2*x+d)/2, (2*x-d)/2
-    if x-sol1 > 0 && sol1 % 1 == 0
-      answer << x.to_i
-      answer << (x-sol1).to_i
-    end
-    if x-sol2 > 0 && sol2 % 1 == 0
-      answer << x
-      answer << (x-sol2).to_i
+    if d % 1 == 0
+      sol1, sol2 = (2*x+d)/2, (2*x-d)/2
+      if x-sol1 > 0
+        answer << x.to_i
+        answer << (x-sol1).to_i
+      end
+      if x-sol2 > 0
+        answer << x
+        answer << (x-sol2).to_i
+      end
     end
     # p "#{x}, #{sol1}, #{sol2}"
   end
