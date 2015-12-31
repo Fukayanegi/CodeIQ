b4225fee4e58d504ff5deb70500121a0383859e6
Fukayanegi <fukayanegi1981@gmail.com>
2015-12-31 22:41:58 +0900

    inprove algorithm #14

--- square_quartet.rb
+++ square_quartet.rb
@@ -1,16 +1,22 @@
 a, b = STDIN.gets.chomp.split(' ').map {|obj| obj.to_i }
 diff = b**2-a**2
-x = Math.sqrt(diff).ceil
-y = Math.sqrt(x**2-diff)
+
 answer = []
 
-while y.ceil<x do 
-  if y % 1 == 0
-    answer << x
-    answer << y.to_i
+fin = (diff+1)/2
+(2..fin).each do |x|
+  if 4*x**2-4*diff > 0
+    d = Math.sqrt(4*x**2-4*diff)
+    sol1, sol2 = (2*x+d)/2, (2*x-d)/2
+    if x-sol1 > 0 && sol1 % 1 == 0
+      answer << x.to_i
+      answer << (x-sol1).to_i
+    end
+    if x-sol2 > 0 && sol2 % 1 == 0
+      answer << x
+      answer << (x-sol2).to_i
+    end
   end
-  x += 1
-  y = Math.sqrt(x**2-diff)
 end
 
 puts answer.inject(:+)
