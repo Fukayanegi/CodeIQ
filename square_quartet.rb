4917fe8b39697489f3a0f3979bebda0270a8c883
Fukayanegi <fukayanegi1981@gmail.com>
2015-12-31 09:39:19 +0900

    implement code which is able to solve sample #14

--- square_quartet.rb
+++ square_quartet.rb
@@ -1,6 +1,16 @@
 a, b = STDIN.gets.chomp.split(' ').map {|obj| obj.to_i }
-p "#{a}, #{b}"
 diff = b**2-a**2
-init = Math.sqrt(diff).ceil
-p init
+x = Math.sqrt(diff).ceil
+y = Math.sqrt(x**2-diff)
+answer = []
 
+while y.ceil<x do 
+  if y % 1 == 0
+    answer << x
+    answer << y.to_i
+  end
+  x += 1
+  y = Math.sqrt(x**2-diff)
+end
+
+puts answer.inject(:+)
