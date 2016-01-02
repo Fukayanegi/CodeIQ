454103486bd25706d4d7824a1c17e6878362bb6b
Fukayanegi <fukayanegi1981@gmail.com>
2016-01-02 22:31:23 +0900

    implement code which is able to solove question Closes #14

--- square_quartet.rb
+++ square_quartet.rb
@@ -8,7 +8,7 @@ x = Math.sqrt(diff).ceil
 
 while x <= fin 
   d = Math.sqrt(x**2-diff)
-  if d % 1 == 0
+  if d % 1 == 0 && d > 0
     answer << x.to_i
     answer << d.to_i
 
@@ -16,7 +16,7 @@ while x <= fin
     break if terms <= 0 || x >= fin
 
     a = 0.1
-    while (a % 1 != 0) && (x <= fin) && (terms > 0)
+    while ((a+1)%2 != 0) && (x <= fin) && (terms > 0)
       a = (diff+terms**2-terms)/terms
       x = (a+1)/2
       # p "#{a}, #{x}, #{terms}, #{diff}, #{fin}"
