d596ace14bb6ee516dbe0e6e6eb3c4d0e84f0caf
Fukayanegi <fukayanegi1981@gmail.com>
2016-01-24 22:58:43 +0900

    prametize keyword Closes #24

--- split_sentence_of_English.rb
+++ split_sentence_of_English.rb
@@ -1,5 +1,13 @@
 document = STDIN.gets
-regex = Regexp.new(".*?\\D+?.*?(?<!Mr)(?<!Ms)(?<!Mrs)(?<!Mt)(?:\\.+|\\?+|\\!+|\\.+)(?: |\\Z)")
+keyword_end_before = ["\\.", "\\?", "\\!"]
+keyword_end = [" ", "\\Z"]
+keyword_not_end = ["Mr", "Ms", "Mrs", "Mt"]
+
+str_keb = "(?:" + keyword_end_before.map {|keyword| "#{keyword}+" }.join("|") + ")"
+str_ke = "(?:" + keyword_end.map {|keyword| "#{keyword}" }.join("|") + ")"
+str_ne = keyword_not_end.map {|keyword| "(?<!#{keyword})" }.join 
+
+regex = Regexp.new(".*?\\D+?.*?" + str_ne + str_keb + str_ke)
 
 matches = document.scan(regex)
 
