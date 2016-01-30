e33b7def3d91d58a8f19733c06cc150a51d262b2
Fukayanegi <fukayanegi1981@gmail.com>
2016-01-30 23:23:06 +0900

    add module Fibonacci and implemment code which translate row and column value to index #28

--- Its_not_Pascals_triangle.rb
+++ Its_not_Pascals_triangle.rb
@@ -1,4 +1,23 @@
+base_row = "a".ord
+base_col = "A".ord
+
 position = STDIN.gets.chomp!
 row , col = position[0], position[1]
 
-p "#{row}, #{col}"
+# from http://blog.h13i32maru.jp/entry/20101016/1287227174
+module Fibonacci
+  @@memo = [0 , 1];
+
+  def self.[](n)
+    if @@memo.size <= n
+      @@memo[n - 1] = self[n - 1] unless @@memo[n - 1];
+      @@memo[n] = @@memo[n - 2] + @@memo[n - 1];
+    end
+
+    return @@memo[n];
+  end
+end
+
+fib = Fibonacci[26]
+
+p "#{row.ord - base_row}, #{col.ord - base_col}"
