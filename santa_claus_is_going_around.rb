7d83cc177fc1ab377b228cd9914593e29c1510ae
Fukayanegi <fukayanegi1981@gmail.com>
2016-01-30 09:40:17 +0900

    implement code which is able to solve all tests Closes #26

--- santa_claus_is_going_around.rb
+++ santa_claus_is_going_around.rb
@@ -10,14 +10,10 @@ def calc_best_route houses, present
   c_houses = houses.length
   return [] if c_houses == 0
   return houses.dup if c_houses == 1
-  # return [11,1,10,2,9,3,6,4,7,5,8] if houses == [1,2,3,4,5,6,7,8,9,10,11]
 
   min_energy = Float::INFINITY
-  key = "#{present}:#{houses.sort.join("")}"
+  key = "#{present}:#{houses.sort.join(",")}"
   best_route = []
-
-  # p "#{key}, #{houses}"
-  # sleep(1)
 
   if @memo.include? key
     best_route = @memo[key] 
@@ -28,34 +24,17 @@ def calc_best_route houses, present
       route = ([] << next_house).concat(calc_best_route houses, next_house)
       energy = present * next_house * route.inject(:+) + calc_energy(route)
 
-      # p "route: #{route} ,#{next_house}, #{route} : #{energy}" #if next_house == 4 && houses.length == 3
+      # p "route: #{route} ,#{next_house}, #{route} : #{energy}"
       min_energy, best_route = [energy, route] if min_energy > energy
 
       houses.insert next_i, next_house
-      # p "best_route: #{best_route}" if c_houses == 5
-      # p min_energy      
     end
 
-    # p "#{key} ::  #{best_route}"
+    # p "#{key} ::  #{best_route}" if c_houses == 11
     @memo[key] = best_route
   end
 
   best_route
-end
-
-def calc_min_energy frist, rest
-  route = ([]<<first).concat(rest)
-  key = route.sort.join("")
-  if @memo.include? key
-    calc_energy @memo[key]
-  else
-    min_energy = Float::INFINITY
-    rest.permutaiton.each do |perm|
-      energy = calc_energy [[]<<first].concat(perm)
-      min_energy = energy if min_energy > energy
-    end
-    min_energy
-  end
 end
 
 def calc_energy route
@@ -71,5 +50,5 @@ end
 
 route = calc_best_route houses, 0
 
-p route
+# p route
 puts calc_energy route
