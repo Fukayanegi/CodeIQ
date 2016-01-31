f7f5a7127a37a927d37122de67b35dd6ec0ca475
Fukayanegi <fukayanegi1981@gmail.com>
2016-02-01 00:51:23 +0900

    modifiy algorithm with memoizaition Closes #30

--- Its_not_Pascals_triangle.rb
+++ Its_not_Pascals_triangle.rb
@@ -1,22 +1,38 @@
-base_row = "a".ord
-base_col = "A".ord
-c_row = "z".ord - base_row + 1
-c_col = "Z".ord - base_col + 1
+# from http://blog.h13i32maru.jp/entry/20101016/1287227174
+module Fibonacci
+  @@memo = [0 , 1];
 
-position = STDIN.gets.chomp!
-row , col = position[0].ord - base_row, position[1].ord - base_col
+  def self.[](n)
+    if @@memo.size <= n
+      @@memo[n - 1] = self[n - 1] unless @@memo[n - 1];
+      @@memo[n] = @@memo[n - 2] + @@memo[n - 1];
+    end
 
-table = [] << Array.new(c_col) { 0 }
-
-c_row.times do |row|
-  row_tmp = [1]
-  first = 0
-  second = 1
-  (c_col-1).times do |col|
-    first, second = second, (first + second + table[-1][col+1])
-    row_tmp << second
+    return @@memo[n];
   end
-  table << row_tmp
 end
 
-puts table[row+1][col]
+@memo = Hash.new
+
+def solve row, col
+  return 0 if col < 1
+  return 1 if col == 1
+  return Fibonacci[col] if row == 0
+  key = "#{row}:#{col}"
+  return @memo[key] if @memo.include? key
+
+  value_previous = solve row, col - 1
+  value_previous2 = solve row, col - 2
+  value_diff = solve row - 1, col
+
+  @memo[key] = value_previous2 + value_previous + value_diff
+  return @memo[key]
+end
+
+base_row = "a".ord
+base_col = "A".ord
+
+position = STDIN.gets.chomp!
+row , col = position[0].ord - base_row, position[1].ord - base_col + 1
+
+puts solve row, col
