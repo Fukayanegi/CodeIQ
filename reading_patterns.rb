acbca778b11d9e883c09f37f6cd128aa52096ec1
Fukayanegi <fukayanegi1981@gmail.com>
2016-02-02 16:17:02 +0900

    implement code whici is able to solve 100,2 but contains bug #34

--- reading_patterns.rb
+++ reading_patterns.rb
@@ -1,3 +1,30 @@
-pages, days = STDIN.gets.chomp!.split(',')
+@memo_patterns = Hash.new
 
-p "#{pages}, #{days}"
+def solve_reading_patterns pages, limit
+  return 1 if limit == 1
+  key = "#{pages}:#{limit}"
+  return @memo_patterns[key] if @memo_patterns.include? key 
+
+  todays_limit = pages - (limit * (limit - 1)) / 2
+  todays_quota = ((limit.to_f**2-limit+2*pages)/(2*limit)).ceil
+
+  # p "****pages: #{pages}, limit: #{limit}****"
+  # p "limit: #{todays_limit}"
+  # p "quota: #{todays_quota}"
+
+  patterns = 0
+  (todays_quota..todays_limit).each do |page|
+    patterns += solve_reading_patterns pages-page, limit-1
+  end
+
+  @memo_patterns[key] = patterns
+  patterns
+end
+
+pages, limit = STDIN.gets.chomp!.split(',').map{|num| num.to_i}
+
+answer = 0
+(1..limit).each do |days|
+  answer += solve_reading_patterns pages, days
+end
+puts answer
