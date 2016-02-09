d3b92c23b280b0c4af4a5d4bb77b8bb8d5f15325
Fukayanegi <fukayanegi1981@gmail.com>
2016-02-09 22:15:17 +0900

    implement code which is base #41

--- janken_assigned_times.rb
+++ janken_assigned_times.rb
@@ -1,3 +1,22 @@
-a, b, max_times = STDIN.gets.chomp!.split(',')
+a, b, max_tries = STDIN.gets.chomp!.split(',').map{|value| value.to_i}
 
-p "#{a}, #{b}, #{max_times}"
+@memo = Hash.new
+@route = Array.new
+def num_of_pattern coins, coins_for_win, c_try, max_tries
+  key = "#{c_try}:#{coins}:#{max_tries}"
+  return @memo[key] if @memo.include? key
+
+  return 1 if (coins == 1 || coins == coins_for_win-1) && max_tries == 1
+  return 0 if (coins > max_tries) && (coins < coins_for_win - max_tries)
+
+  answer = 0
+  answer += num_of_pattern coins-1, coins_for_win, c_try+1, max_tries-1 if coins > 1
+  answer += num_of_pattern coins+1, coins_for_win, c_try+1, max_tries-1 if coins < coins_for_win-1
+
+  @memo[key] = answer
+  answer
+end
+
+p "#{a}, #{b}, #{max_tries}"
+p num_of_pattern a, a+b, 0, max_tries
+p @memo
