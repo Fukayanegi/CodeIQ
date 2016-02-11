97b2cdecc374c05a23b6ce078b068c3cf177aa52
Fukayanegi <fukayanegi1981@gmail.com>
2016-02-11 22:23:23 +0900

    completely reviesd algorithm by recursion to loop Closes #43

--- square_from_rectangle.rb
+++ square_from_rectangle.rb
@@ -2,43 +2,21 @@ max_side_length, TARGET_SQUARES = STDIN.gets.chomp!.split(',').map{|val| val.to_
 
 @memo = Hash.new
 
-# 再帰呼び出しの度にnum_of_squaresをデクリメントしていき、
-# width == height、またはnum_of_square == 1のときの状態で、切り取れる正方形の数を返す
-# width != height、かつnum_of_suquares == 1の場合、TARGET_SQUARES+1を返すことで呼び出し元で条件に合わない形とさせる
-def cut_squares width, height, num_of_squares
-  width, height = height, width if height > width
-  key = "#{width}:#{height}"
-  # @memo[key] if @memo.include? key
-  if @memo.include? key
-    # p "#{key}: #{@memo[key]}"
-    @memo[key]
-  end
-
-  return 1 if width == height
-  return TARGET_SQUARES if num_of_squares == 1
-
-  @memo[key] = (cut_squares width - height, height, num_of_squares - 1) + 1
-  # p "memoization: #{key}: #{@memo[key]}"
-  @memo[key]
-end
-
-width = Array.new(max_side_length) {|i| max_side_length - i}
-# p width
-
-answer = 0
-if max_side_length != 1000
-  d_rectangles = 0
-  # 最初の幅、高さが決まれば後は決まった処理の繰り返し
+def count_patterns2 max_side_length
+  answer = 0
   (1..max_side_length).each do |w|
     (1..(w - 1)).each do |h|
-      d_rectangles += 1
       # p "#{w}, #{h}"
-      squares = cut_squares w, h, TARGET_SQUARES
+      squares = 0
+      tmp_w, tmp_h = w, h
+      while tmp_h != 0 do
+        squares += tmp_w / tmp_h
+        tmp_w, tmp_h = tmp_h, tmp_w - tmp_h * (tmp_w / tmp_h)
+      end
       answer += 1 if squares == TARGET_SQUARES
-      # p "#{w}, #{h}" if squares == TARGET_SQUARES
     end
   end
+  answer
 end
 
-# p "rectangles: #{d_rectangles}"
-p answer
+p count_patterns2 max_side_length
