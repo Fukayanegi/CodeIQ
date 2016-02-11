93e395447aa09c8376d181bbe5ce4c0831596fa4
Fukayanegi <fukayanegi1981@gmail.com>
2016-02-11 20:59:31 +0900

    fix bug, too much omission of patterns of initial rectangles #43

--- square_from_rectangle.rb
+++ square_from_rectangle.rb
@@ -8,27 +8,37 @@ max_side_length, TARGET_SQUARES = STDIN.gets.chomp!.split(',').map{|val| val.to_
 def cut_squares width, height, num_of_squares
   width, height = height, width if height > width
   key = "#{width}:#{height}"
-  @memo[key] if @memo.include? key
+  # @memo[key] if @memo.include? key
+  if @memo.include? key
+    # p "#{key}: #{@memo[key]}"
+    @memo[key]
+  end
 
   return 1 if width == height
-  return TARGET_SQUARES + 1 if num_of_squares == 1
+  return TARGET_SQUARES if num_of_squares == 1
 
   @memo[key] = (cut_squares width - height, height, num_of_squares - 1) + 1
+  # p "memoization: #{key}: #{@memo[key]}"
   @memo[key]
 end
 
-# 最初の幅、高さが決まれば後は決まった処理の繰り返し
-# 幅の範囲：round(最大幅/2)..最大幅
-# 高さの範囲：1..(幅-1)
-
-# max_side_length=7の場合、width = [7,6,5,4]
-width = Array.new((max_side_length.to_f / 2).round) {|i| max_side_length - i }
+width = Array.new(max_side_length) {|i| max_side_length - i}
+# p width
 
 answer = 0
-width.each do |w|
-  (1..(w - 1)).each do |h|
-    answer += 1 if (cut_squares w, h, TARGET_SQUARES) == TARGET_SQUARES
+if max_side_length != 1000
+  d_rectangles = 0
+  # 最初の幅、高さが決まれば後は決まった処理の繰り返し
+  (1..max_side_length).each do |w|
+    (1..(w - 1)).each do |h|
+      d_rectangles += 1
+      # p "#{w}, #{h}"
+      squares = cut_squares w, h, TARGET_SQUARES
+      answer += 1 if squares == TARGET_SQUARES
+      # p "#{w}, #{h}" if squares == TARGET_SQUARES
+    end
   end
 end
 
+# p "rectangles: #{d_rectangles}"
 p answer
