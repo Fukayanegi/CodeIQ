1be99e6a505ebfbbb59c756ce87a117d352e8247
Fukayanegi <fukayanegi1981@gmail.com>
2016-02-12 23:51:52 +0900

    implement code which is able to solve all tests Colses #45

--- daisy_chained_retire.rb
+++ daisy_chained_retire.rb
@@ -1,13 +1,21 @@
-input = Array.new(11)
-11.times do |i|
-  input[i] = STDIN.gets.chomp!
+input = Hash.new
+first = STDIN.gets.chomp!
+9.times do
+  key, value = STDIN.gets.chomp!.split(",")
+  input[key] = value
 end
 
 def count_menber_of_retire first_retired, members
-  p first_retired
-  p members
-  0
+  retired = [first_retired]
+
+  while members.select {|k, v| retired.include? v}.count != 0 do
+    retired_tmp = members.select {|k, v| retired.include? v}.keys
+    members.select! {|k, v| !(retired.include? v)}
+    retired = retired_tmp
+    # p "retired: #{retired}"
+  end
+
+  members.count
 end
 
-first = input.shift
 puts count_menber_of_retire first, input
