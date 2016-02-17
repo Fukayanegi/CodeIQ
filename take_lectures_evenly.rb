86bacf1a44a7308c7e40a7d4ca1db66fecfcb2e6
Fukayanegi <fukayanegi1981@gmail.com>
2016-02-17 22:00:11 +0900

    wrap the class the code to solve question #50

--- take_lectures_evenly.rb
+++ take_lectures_evenly.rb
@@ -1,8 +1,20 @@
 venues, classes = STDIN.gets.chomp!.split(",").map{|v| v.to_i}
 p "#{venues}, #{classes}"
 
-# 各会場で等しい回数受講するセッション数の最大値分繰り返す
-even_take_max = classes / venues
-(0..even_take_max).each do |take|
-  p take
+class Solver
+  def initialize venues, classes
+    @venues = venues
+    @classes = classes
+  end
+
+  def solve
+    # 各会場で等しい回数受講するセッション数の最大値分繰り返す
+    even_take_max = @classes / @venues
+    (0..even_take_max).each do |take|
+      p take
+    end
+  end
 end
+
+solver = Solver.new venues, classes
+solver.solve
