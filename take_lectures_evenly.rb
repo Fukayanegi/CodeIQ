f6332bef1db0bec4072c62de554556e93456fcc8
Fukayanegi <fukayanegi1981@gmail.com>
2016-02-17 21:55:48 +0900

    loop max times of taking lectures evenly at each venues #50

--- take_lectures_evenly.rb
+++ take_lectures_evenly.rb
@@ -1,2 +1,8 @@
 venues, classes = STDIN.gets.chomp!.split(",").map{|v| v.to_i}
 p "#{venues}, #{classes}"
+
+# 各会場で等しい回数受講するセッション数の最大値分繰り返す
+even_take_max = classes / venues
+(0..even_take_max).each do |take|
+  p take
+end
