478ade8b125fb4eb51c3daae97e5eefd50a59402
Fukayanegi <fukayanegi1981@gmail.com>
2016-02-18 00:45:12 +0900

    revise algorithm but including bugs #53

--- programming_club/count_up_7s.rb
+++ programming_club/count_up_7s.rb
@@ -8,9 +8,26 @@ class Solver
   def solve
     answer = 0
     
-    range = Range.new(1, @range_to)
-    range.each do |num|
-      answer += num.to_s.scan(@@target).count
+    # range = Range.new(1, @range_to)
+    # range.each do |num|
+    #   answer += num.to_s.scan(@@target).count
+    # end
+
+
+    digit = @range_to.to_s.length - 1
+    max_ditit_limit = @range_to / (10 ** digit)
+    # p "#{digit}, #{max_ditit_limit}"
+
+    digits = (1..digit).to_a
+    (1..digit).each do |select_num|
+      answer += digits.combination(select_num).to_a.count * select_num
+    end
+    # p "#{answer}"
+
+    if max_ditit_limit >= 7
+      answer = (max_ditit_limit + 1) * answer + 10 ** digit
+    else
+      answer = max_ditit_limit * answer
     end
 
     answer
