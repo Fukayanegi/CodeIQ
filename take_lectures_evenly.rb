049aa9fac522857802d86b78c7d059a1abe70d4f
Fukayanegi <fukayanegi1981@gmail.com>
2016-02-17 22:21:15 +0900

    count up permutations by loop #50

--- take_lectures_evenly.rb
+++ take_lectures_evenly.rb
@@ -8,13 +8,21 @@ class Solver
   end
 
   def solve
+    answer = 0
+
     # 各会場で等しい回数受講するセッション数の最大値分繰り返す
     even_take_max = @classes / @venues
-    (0..even_take_max).each do |take|
-      p take
+    taiking_patterns = Range.new(0, even_take_max)
+    taiking_patterns.each do |take|
+      # 受講数（各会場で等しい回数受講可能なセッション数*会場数）をセッション数の中から順列で取得する
+      classes = Range.new(1, @classes)
+      answer += classes.to_a.permutation(take * @venues).to_a.length
+      # p "tmp_anser: #{answer} at #{take}"
     end
+    # p answer
+    return answer
   end
 end
 
 solver = Solver.new venues, classes
-solver.solve
+p solver.solve
