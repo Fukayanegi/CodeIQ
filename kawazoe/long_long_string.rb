91d2ea54babaee828a892dd6acb3438df71b5b26
Fukayanegi <fukayanegi1981@gmail.com>
2016-02-19 23:57:35 +0900

    handle the case of answer -1 #55

--- kawazoe/long_long_string.rb
+++ kawazoe/long_long_string.rb
@@ -5,13 +5,15 @@ class Solver
 
   def solve
     # m+1 > n * Math.log10(n) + 1 >= m となるnを求める
-    answer = 1
+    answer = -1
 
-    while (judge_value = answer * Math.log10(answer) + 1) < @m + 1 do
+    answer_tmp = 1
+    while (judge_value = answer_tmp * Math.log10(answer_tmp) + 1) < @m + 1 do
       if judge_value < @m + 1 && judge_value >= @m
+        answer = answer_tmp
         break
       end
-      answer += 1
+      answer_tmp += 1
     end
 
     return answer
