ce472ba1787d5bdf83b9b10edab61ffd35aeb1c9
Fukayanegi <fukayanegi1981@gmail.com>
2016-02-19 23:54:17 +0900

    fix bug base count is wrong #55

--- kawazoe/long_long_string.rb
+++ kawazoe/long_long_string.rb
@@ -4,10 +4,10 @@ class Solver
   end
 
   def solve
-    # m+1 > n * Math.log10(n) >= m となるnを求める
+    # m+1 > n * Math.log10(n) + 1 >= m となるnを求める
     answer = 1
 
-    while (judge_value = answer * Math.log10(answer)) < @m + 1 do
+    while (judge_value = answer * Math.log10(answer) + 1) < @m + 1 do
       if judge_value < @m + 1 && judge_value >= @m
         break
       end
