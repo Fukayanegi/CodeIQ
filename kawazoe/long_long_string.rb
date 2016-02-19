508b8d44ab1cce8636fe701be561dcaa252c21d5
Fukayanegi <fukayanegi1981@gmail.com>
2016-02-19 23:28:36 +0900

    create class of Solver #55

--- kawazoe/long_long_string.rb
+++ kawazoe/long_long_string.rb
@@ -1,1 +1,14 @@
+class Solver
+  def initialize m
+    @m = m
+  end
+
+  def solve
+    answer = 0
+    return answer
+  end
+end
+
 m = STDIN.gets.chomp!.to_i
+solver = Solver.new m
+p solver.solve
