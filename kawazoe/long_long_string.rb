b5ea4a913606d474e4218385ba6b39fd67cf9d87
Fukayanegi <fukayanegi1981@gmail.com>
2016-02-20 23:13:12 +0900

    consider n of initial value #55

--- kawazoe/long_long_string.rb
+++ kawazoe/long_long_string.rb
@@ -7,7 +7,14 @@ class Solver
     # m+1 > n * Math.log10(n) + 1 >= m となるnを求める
     answer = -1
 
+    # 1からインクリメントして探索
     answer_tmp = 1
+
+    while answer_tmp * 10 * Math.log10(answer_tmp * 10) < @m - 2 do
+      answer_tmp = answer_tmp * 10
+    end
+
+    p "#{answer_tmp}"
     while (judge_value = answer_tmp * Math.log10(answer_tmp) + 1) < @m + 1 do
       if judge_value < @m + 1 && judge_value >= @m
         answer = answer_tmp
