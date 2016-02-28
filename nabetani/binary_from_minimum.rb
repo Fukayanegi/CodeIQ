07f472f0b96a58bece1c6a49093b8e4788cc4e6c
Fukayanegi <fukayanegi1981@gmail.com>
2016-02-28 21:50:40 +0900

    implement: self.factrial and self.combination #66

--- nabetani/binary_from_minimum.rb
+++ nabetani/binary_from_minimum.rb
@@ -6,9 +6,30 @@ class Solver
     @y = y
   end
 
+  def self.factorial(number)
+    number = 0 if number.nil?
+    (1..number).inject(1,:*)
+  end
+
+  def self.combination n, r
+    self.factorial(n) / self.factorial(r)
+  end
+
   def solve
+    patterns = 0
+    incremental = 0
+    digits = @x - 1
+    while patterns + incremental < @y do
+      digits += 1
+      patterns += incremental
+
+      incremental = Solver.combination digits, @x
+      p "#{digits}, #{@x}, #{incremental}"
+    end
+
+    ("0b" + "1"*digits)
   end
 end
 
 solver = Solver.new x, y
-solver.solve
+p solver.solve
