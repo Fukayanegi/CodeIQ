ea1527d5d0aa726feba3702a746f93d8c78b0c70
Fukayanegi <fukayanegi1981@gmail.com>
2016-03-03 23:32:31 +0900

    implement: basecode #66

--- nabetani/binary_from_minimum.rb
+++ nabetani/binary_from_minimum.rb
@@ -1,9 +1,9 @@
 x, y = STDIN.gets.chomp!.split(",").map{|value| value.to_i}
 
 class Solver
-  def initialize x, y
+  def initialize x
     @x = x
-    @y = y
+    @memo = Hash.new
   end
 
   def self.power num
@@ -11,44 +11,70 @@ class Solver
     2 ** num
   end
 
-  # def self.factorial(number)
-  #   return 1 if number <= 1
-  #   number = 0 if number.nil?
-  #   (1..number).inject(1,:*)
-  # end
+  # n桁のbinaryの中に最大@x桁1が連続するものの数
+  def count_target_pattern n, recursive
+    n = 0 if n < 0
+    return 2 ** n if (n <= @x && recursive == 1)
+    return @memo[n] if @memo.include? n
 
-  # def self.combination n, r
-  #   self.factorial(n) / self.factorial(r)
-  # end
+    # N-i..N-i-@x桁目が1でN-i+1桁目が0かつN-i-@x-1桁目が0となるものの累積
+    answer = 0
+    0.upto(n - @x).each do |i|
+      left = count_target_pattern i - 1, 1
+      right = count_target_pattern(n - i - @x - 1, 1)
+      answer += left * right
+    end
+    @memo[n] = answer
+    answer
+  end
 
-  def solve
+  # n桁以下の数の中で最大@x桁1が連続する数のp番目の数
+  def solve_nmy n, y
+    return 1
+  end
+
+  #　最大m桁1が連続する数がorder以下になる桁数とパターン数
+  def solve_digits order
     patterns = 0
-    incremental = 0
     digits = @x - 1
 
-    while patterns + incremental < @y do
+    while patterns < order
+      # 繰り返し実行の最初に前回結果を累積する
       digits += 1
-      patterns += incremental
 
-      # N桁の2進数を考えたとき、
-      # N-i..N-i-@x桁目が1でN-i+1桁目が1とならないかつN-i-@x-1桁目が1とならないケースが
-      # 最大1が@x桁続く数字
-      incremental = 0
-      (0..digits-@x).each do |i|
-        left = Solver.power i - 1
-        right = Solver.power(digits - i - @x - 1)
-        incremental += left * right
-      end
-      # 前回の桁数からの増分を記録する
-      incremental -= patterns
+      patterns = count_target_pattern digits, 0
 
-      p "#{digits}, #{@x}, #{incremental}, #{patterns}"
+      p "#{digits}, #{patterns}"
     end
 
-    # digits桁の中に@y番目の数がある
-    ("0b" + "1"*digits)
+    return digits, patterns
+  end
+
+  def solve order
+    digits, patterns = solve_digits order
+
+    return order if order <= ("1"*@x).to_i(2)
+
+    rest_patterns = order - patterns
+    num = 1 << (digits + 1)
+
+    while rest_patterns > 0
+      1.upto(@x).each do |d|
+        patterns = count_target_pattern(digits - d)
+        if patterns > rest_patterns
+          d - 1
+          digits = digits - d
+          break
+        end
+        rest_patterns -= patterns
+      end
+      num
+    end
+
+    num
   end
 end
 
-solver = Solver.new x, y
-p solver.solve
+solver = Solver.new x
+# p solver.solve y
+p solver.solve_digits 29
