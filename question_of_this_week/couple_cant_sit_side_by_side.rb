f82f4a9693f242c053f9353eeae526f25577e64b
Fukayanegi <fukayanegi1981@gmail.com>
2016-03-10 23:38:58 +0900

    implement: can solve sample question #70

--- question_of_this_week/couple_cant_sit_side_by_side.rb
+++ question_of_this_week/couple_cant_sit_side_by_side.rb
@@ -1,12 +1,44 @@
-couples = STDIN.gets.chomp!
+couples = STDIN.gets.chomp!.to_i
 
 class Solver
   def initialize couples
-    @couples = couples
+    @couples = Array.new
+    couples.times do |i| 
+      @couples << i
+      @couples << i
+    end
+    @seat = Array.new(couples * 2)
+  end
+
+  def count_patterns seat, couples, sex
+    # p "count_patterns >> #{seat}, #{couples}, #{sex}"
+    return 1 if couples.length == 0
+
+    patterns = 0
+
+    lover = couples.shift
+    # p "lover: #{lover}"
+    (@seat.length / 2).times do |i|
+      seat_i = 2 * i + sex
+      left_i, right_i = (seat_i - 1) % seat.length, (seat_i + 1) % seat.length
+
+      # p "left >> index: #{left_i}, value: #{seat[left_i]}"
+      # p "right >> index: #{right_i}, value: #{seat[right_i]}"
+      if seat[left_i] != lover && seat[right_i] != lover && seat[seat_i].nil?
+        seat[seat_i] = lover 
+        next_sex = sex == 0 ? 1 : 0
+        patterns += count_patterns seat, couples, next_sex
+        seat[seat_i] = nil
+      end
+    end
+    couples.push lover
+    patterns
   end
 
   def solve
-    1
+    @seat[0] = @couples.shift 
+
+    count_patterns @seat, @couples, 1
   end
 end
 
