22c3fb59a857822ec7c087da2780f8ce49b37b12
Fukayanegi <fukayanegi1981@gmail.com>
2016-03-12 23:38:43 +0900

    implement: can solve all questions Closes #72

--- others/best_effective_attribute.rb
+++ others/best_effective_attribute.rb
@@ -8,8 +8,13 @@ attribute = STDIN.gets.chomp
 attribute = attribute == "F" ? 0 : attribute == "W" ? 1 : 2
 values = STDIN.gets.chomp.split(" ").each_with_index.map{|val, i| adjust_value(val.to_i, i, attribute)}
 
+# p attribute
+# p values
 
-p attribute
-p values
-
-p values.max
+ATTRIBUTE = ["F", "W", "T"]
+answer = []
+values.each_with_index do |val, i|
+  answer << ATTRIBUTE[i] if val == values.max
+end
+puts answer.join
+# p values.inject([]){|ary, val| ary << val if val == values.max; ary}
