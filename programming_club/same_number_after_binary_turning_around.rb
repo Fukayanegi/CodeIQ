cabc35fb3b478f0430053871edb711f7694af199
Fukayanegi <fukayanegi1981@gmail.com>
2016-03-15 08:58:27 +0900

    inplement: prepare for solve #80

--- programming_club/same_number_after_binary_turning_around.rb
+++ programming_club/same_number_after_binary_turning_around.rb
@@ -1,2 +1,19 @@
-from, to = STDIN.gets.chomp.split(",")
-p "#{from}, #{to}"
+class Solver
+  def initialize from, to
+    @from = from
+    @to = to
+  end
+
+  def solve
+    under_digit = @from.to_s(2).length
+    upper_digit = @to.to_s(2).length
+    p "digits: #{under_digit}, #{upper_digit}"
+  end
+end
+
+from, to = STDIN.gets.chomp.split(",").map{|val| val.to_i}
+p "input: #{from}, #{to}"
+p "input_by_binary: #{from.to_s(2)}, #{to.to_s(2)}"
+
+solver = Solver.new from, to
+solver.solve
