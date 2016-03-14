e3ecb71dce52a1e42cb62ea0a54638cdf024c037
Fukayanegi <fukayanegi1981@gmail.com>
2016-03-14 23:43:52 +0900

    improve: by censoring judgement whith previous quotient Closes #78

--- programming_club/count_up_prime_number.rb
+++ programming_club/count_up_prime_number.rb
@@ -7,7 +7,17 @@ class Solver
   def solve
     (2...@limit).each do |num|
       is_prime = true
+      quotient = @limit
       @primes.each do |prime|
+        # p "#{num}, #{prime} >> prime > quotient : #{prime > quotient}"
+        # 判定対象が前回判定時の商を超える素数だった場合、
+        # 以降の商は前回の素数を下回る（既に存在しないことが判定済）ため打ち切り
+        if prime > quotient
+          is_prime = true
+          break
+        end
+        quotient = num / prime
+        # p "#{num}, #{prime} >> num % prime : #{num % prime}"
         if num % prime == 0
           is_prime = false
           break
@@ -15,7 +25,8 @@ class Solver
       end
       @primes << num if is_prime
     end
-    @primes
+    # p @primes
+    @primes.length
   end
 end
 
