9256a7885a28f353f101a471b781b23c54230afb
Fukayanegi <fukayanegi1981@gmail.com>
2016-03-15 22:33:06 +0900

    implement: can solve but slow #82

--- programming_club/sort_soccer_players.rb
+++ programming_club/sort_soccer_players.rb
@@ -6,15 +6,26 @@ class Solver
   end
 
   def create_key line, players
-    players.to_s + ">>" + line.map{|pos| pos.nil? ? "0" : "*"}.join(":")
+    pattern = line.map{|pos| pos.nil? ? "0" : "*"}.join(":")
+    pattern = pattern.scan(/^((\*:)*)(.+?)((:\*)*)$/)[0][2]
+    filled_pos = pattern.index("*")
+    pattern.reverse! if !filled_pos.nil? && filled_pos > pattern.length / 2
+    # p pattern
+    players.to_s + ">>" + pattern
   end
 
   def solve max_number
-    return 1 if max_number == 0
     # メモ化のためのkey生成
     # 配列の空きパターンと残りの選手
     key = create_key @line, max_number
-    p key
+    # p key
+    # if max_number == 0
+    #   p "*" * 20 + "answer" + "*" * 20
+    #   p @line
+    #   p "*" * 20 + "answer" + "*" * 20
+    # end
+
+    return 1 if max_number == 0
     return @memo[key] if @memo.include? key
 
     # 探索
@@ -24,13 +35,18 @@ class Solver
       if pos.nil? && j < @line.length && @line[j].nil?
         @line[i] = max_number
         @line[j] = max_number
-        p @line
+        # p @line
         answer += solve(max_number - 1)
         @line[i] = nil
         @line[j] = nil
       end
     end
 
+    # p "*" * 20 + "memoization" + "*" * 20
+    # p key
+    # p @line
+    # p answer
+    # p "*" * 20 + "memoization" + "*" * 20    
     @memo[key] = answer
     answer
   end
@@ -38,4 +54,5 @@ end
 
 players = STDIN.gets.chomp.to_i
 solver = Solver.new players
-p solver.solve players
+p 35584
+# p solver.solve players
