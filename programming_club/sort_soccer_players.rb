93b681b8f0a486275cfcb1e2c39e3e3d23342b82
Fukayanegi <fukayanegi1981@gmail.com>
2016-03-15 21:50:46 +0900

    implement: can solve 3players #82

--- programming_club/sort_soccer_players.rb
+++ programming_club/sort_soccer_players.rb
@@ -6,18 +6,29 @@ class Solver
   end
 
   def create_key line, players
-    players.to_s + ">>" + line.join(":")
+    players.to_s + ">>" + line.map{|pos| pos.nil? ? "0" : "*"}.join(":")
   end
 
   def solve max_number
+    return 1 if max_number == 0
     # メモ化のためのkey生成
     # 配列の空きパターンと残りの選手
     key = create_key @line, max_number
+    p key
     return @memo[key] if @memo.include? key
 
     # 探索
     answer = 0
     @line.each_with_index do |pos, i|
+      j = i + max_number + 1
+      if pos.nil? && j < @line.length && @line[j].nil?
+        @line[i] = max_number
+        @line[j] = max_number
+        p @line
+        answer += solve(max_number - 1)
+        @line[i] = nil
+        @line[j] = nil
+      end
     end
 
     @memo[key] = answer
