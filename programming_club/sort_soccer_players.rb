9b6be70f147f2517493275b99428a84d5433cc90
Fukayanegi <fukayanegi1981@gmail.com>
2016-03-17 13:12:16 +0900

    improve: by immadiately returning 0 if can't put any player to target position #82

--- programming_club/sort_soccer_players.rb
+++ programming_club/sort_soccer_players.rb
@@ -1,8 +1,12 @@
 class Solver
+  attr_accessor :hit, :total
+
   def initialize players
     @players = players
     @line = Array.new @players * 2
     @memo = Hash.new
+    @hit = 0
+    @total = 0
   end
 
   def create_key line, players
@@ -15,38 +19,51 @@ class Solver
   end
 
   def solve max_number
+    @total += 1
     # メモ化のためのkey生成
     # 配列の空きパターンと残りの選手
     key = create_key @line, max_number
     # p key
-    # if max_number == 0
-    #   p "*" * 20 + "answer" + "*" * 20
-    #   p @line
-    #   p "*" * 20 + "answer" + "*" * 20
-    # end
 
     return 1 if max_number == 0
-    return @memo[key] if @memo.include? key
+    if @memo.include? key
+      @hit += 1
+      # p "cached answer ***** >> #{key}, #{@memo[key]}"
+      return @memo[key] 
+    end
 
     # 探索
     answer = 0
     @line.each_with_index do |pos, i|
       j = i + max_number + 1
-      if pos.nil? && j < @line.length && @line[j].nil?
+      break if j >= @line.length
+      if pos.nil? && @line[j].nil?
         @line[i] = max_number
         @line[j] = max_number
         # p @line
         answer += solve(max_number - 1)
         @line[i] = nil
         @line[j] = nil
+      elsif pos.nil?
+        # 対象の場所に配置できる選手がいるか確認
+        # いなければこの空きパターンの答えは0
+        # p "possibility ******* >> #{@line}, #{pos}, #{@line[j]}"
+        impossible = true
+        (max_number - 1).downto(1) do |number|
+          k = i + number + 1
+          l = i - (number + 1)
+          break if k >= @line.length && l < 0
+          # p "possibility ******* >> number:#{number}, #{pos}, #{@line[k]}, #{@line[l]}"
+          if (k < @line.length && @line[k].nil?) || (l >= 0 && @line[l].nil?)
+            impossible = false
+            break
+          end
+        end
+        break if impossible
       end
     end
 
-    # p "*" * 20 + "memoization" + "*" * 20
-    # p key
-    # p @line
-    # p answer
-    # p "*" * 20 + "memoization" + "*" * 20    
+    # p "memoization ******* >> #{key}, #{answer}, #{@line}"
     @memo[key] = answer
     answer
   end
@@ -54,5 +71,6 @@ end
 
 players = STDIN.gets.chomp.to_i
 solver = Solver.new players
-p 35584
-# p solver.solve players
+# p 35584
+p solver.solve players
+p "cache hit ********* >> #{solver.total}, #{solver.hit}"
