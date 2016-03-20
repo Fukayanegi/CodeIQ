5e0d0dc589a8c540c031ba5fb470056137582995
Fukayanegi <fukayanegi1981@gmail.com>
2016-03-20 23:36:21 +0900

    inplement: initialize panel #84

--- question_of_this_week/attack_25.rb
+++ question_of_this_week/attack_25.rb
@@ -1,3 +1,11 @@
+def display_panel panel
+  panel.each do |line|
+    p line
+  end
+end
+
+@panel = Array.new(7) {Array.new(7) {0}}
+
 input = Hash.new
 while line = STDIN.gets do
   tmp = line.chomp.split(",")
@@ -5,3 +13,4 @@ while line = STDIN.gets do
 end
 
 p input
+display_panel @panel
