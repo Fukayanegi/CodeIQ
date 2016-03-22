2dbd62f74b614daeef6fa96125fdd7addb551b7a
Fukayanegi <fukayanegi1981@gmail.com>
2016-03-22 22:46:59 +0900

    improve: パターン探索をやめ、答えのパターン数だけを求めるように変更、及びメモ化 Closes #86

--- kawazoe/division_9.rb
+++ kawazoe/division_9.rb
@@ -1,24 +1,37 @@
-# count_of_num個の0,1,2,3の組み合わせでtarget_numとなる順列
-@patterns = []
-def patterns target_num, count_of_num, pattern
-  @patterns << pattern.dup if target_num == 0 && count_of_num == 0
-  (0..3).each do |num|
-    if target_num - num >= 0 && target_num - num <= (count_of_num - 1) * 3
-      pattern << num
-      patterns target_num - num, count_of_num - 1, pattern
-      pattern.slice!(-1)
+class Solver
+  def initialize n
+    @count_of_num = n
+    @min = 1 * n / 9
+    @max = 4 * n / 9
+    @memo = Hash.new
+  end
+
+  # count_of_num個の0,1,2,3の組み合わせでtarget_numとなる順列
+  def count_patterns target_num, count_of_num
+    return 1 if target_num == 0 && count_of_num == 0
+
+    key = "#{target_num}:#{count_of_num}"
+    return @memo[key] if @memo.include? key
+
+    answer = 0
+    (0..3).each do |num|
+      if target_num - num >= 0 && target_num - num <= (count_of_num - 1) * 3
+        answer += count_patterns target_num - num, count_of_num - 1
+      end
     end
+    @memo[key] = answer
+    answer
+  end
+
+  def solve
+    answer = 0
+    (@min..@max).each do |magnification|
+      answer += count_patterns 9 * magnification - @count_of_num, @count_of_num
+    end
+    answer
   end
 end
 
 n = STDIN.gets.chomp.to_i
-p "#{n}"
-
-min, max = 1 * n / 9, 4 * n / 9
-
-(min..max).each do |magnification|
-  patterns 9 * magnification - n, n, []
-end
-
-p @patterns
-p @patterns.length
+solver = Solver.new n
+p solver.solve
