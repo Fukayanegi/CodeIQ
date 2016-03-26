55eb65251bab2d50bbdcf1f18f11b9d245df33d9
Fukayanegi <fukayanegi1981@gmail.com>
2016-03-27 00:20:22 +0900

    implement: 引数解析がいまいちだけど、とりあえず全ての問題が解答可能 #95

--- tp/command_of_shohey.rb
+++ tp/command_of_shohey.rb
@@ -1,2 +1,9 @@
 command = STDIN.gets.chomp!.split(" ")
-p command
+option = Hash.new
+(2..command.length).each do |i|
+  option[command[i]] = true
+end
+
+magnification = option.include?("-e") ? 2 : 1
+question = option.include?("-q") ? "?" : ""
+puts "sho" + ("hey" * (command[1].to_i * magnification)) + question
