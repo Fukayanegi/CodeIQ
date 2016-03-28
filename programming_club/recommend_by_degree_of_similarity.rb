dc02b98ada4e60afd920937467d30105d42109cc
Fukayanegi <fukayanegi1981@gmail.com>
2016-03-29 00:35:56 +0900

    fix: ソートキーを追加 #97
    improve: ベクトル値を事前に計算 #97

--- programming_club/recommend_by_degree_of_similarity.rb
+++ programming_club/recommend_by_degree_of_similarity.rb
@@ -1,27 +1,31 @@
 def display_matrix matrix
   matrix.each do |row|
-    p row.map{|col| "%03s" % col}.join(" ")
+    puts row.map{|col| "%03s" % col}.join("\t")
   end
 end
 
 def recommend_by_similarity item, matrix, c_user
   similarity = Hash.new
+  # mole1 = matrix[item - 1].inject(0){|acm, val| acm += val ** 2}
+  mole1 = matrix[item - 1][-1]
   matrix.each_with_index do |row, i|
     if i != item - 1
       denomi = 0
-      mole1 = 0
-      mole2 = 0
+      # mole2 = 0
+      mole2 = row[-1]
       c_user.times do |col|
-        a = matrix[item - 1][col]
-        b = row[col]
-        denomi += a * b
-        mole1 += a ** 2
-        mole2 += b ** 2
+        denomi += matrix[item - 1][col] * row[col]
+        # a = matrix[item - 1][col]
+        # b = row[col]
+        # denomi += a * b
+        # mole2 += b ** 2
       end
-      similarity[i + 1] = denomi / (Math.sqrt(mole1) * Math.sqrt(mole2))
+      similarity[i + 1] = denomi / Math.sqrt(mole1 * mole2)
+      # p "#{i+1} : #{denomi}, #{mole1}, #{mole2}"
     end
   end
-  similarity.sort{|(k1, v1), (k2, v2)| v2 <=> v1}
+  # p similarity.sort{|(k1, v1), (k2, v2)| v2 <=> v1}
+  similarity.sort{|(k1, v1), (k2, v2)| (v2 <=> v1).nonzero? || (k1 <=> k2)}
 end
 
 input = STDIN.gets.chomp.split(" ").map{|val| val.to_i}
@@ -29,21 +33,32 @@ c_user = input[0]
 c_item = input[1]
 c_rating = input[2]
 
-matrix_rating_row = Array.new(c_user){Array.new(c_item){0}}
-matrix_rating_col = Array.new(c_item){Array.new(c_user){0}}
+# 商品ごとに最後の1項目はuser|a->|の値とする
+matrix_rating_col = Array.new(c_item){Array.new(c_user + 1){0}}
 
 c_rating.times do
   rating = STDIN.gets.chomp.split(" ").map{|val| val.to_i}
-  matrix_rating_row[rating[0] - 1][rating[1] - 1] = rating[2]
   matrix_rating_col[rating[1] - 1][rating[0] - 1] = rating[2]
 end
 
-display_matrix matrix_rating_row
-display_matrix matrix_rating_col
+# display_matrix matrix_rating_row
+# display_matrix matrix_rating_col
+
+start = Time.now
+
+#️ 事前計算
+matrix_rating_col.each do |row|
+  row[-1] = row.inject(0){|acm, val| acm += val **2}
+end
+
+# display_matrix matrix_rating_col
 
 c_recommend = STDIN.gets.chomp.to_i
 c_recommend.times do
   item = STDIN.gets.chomp.to_i
   recommend = recommend_by_similarity item, matrix_rating_col, c_user
+  # puts "#{item} #{recommend[0..2].map{|k, v| k.to_s + ':' + v.to_s}.join(" ")}"
   puts "#{item} #{recommend[0..2].map{|k, v| k}.join(" ")}"
 end
+
+p  "#{Time.now - start}"
