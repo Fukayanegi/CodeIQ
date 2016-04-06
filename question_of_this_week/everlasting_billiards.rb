eba7b6586dc8e55075edb4ef4911c861ed2201d3
Fukayanegi <fukayanegi1981@gmail.com>
2016-04-06 23:09:37 +0900

    refactor: クラス化 #97

--- question_of_this_week/everlasting_billiards.rb
+++ question_of_this_week/everlasting_billiards.rb
@@ -1,72 +1,82 @@
 require 'set'
 
-def display board
-  board.each do |row|
-    p row.map{|c| "%3s" % c}.join(",")
+class Solver
+  def self.display board
+    board.each do |row|
+      p row.map{|c| "%3s" % c}.join(",")
+    end
+  end
+
+  def self.panel x_prev, y_prev, x, y
+    panel_x = x_prev < x ? x_prev : x
+    pnael_y = y_prev < y ? y_prev : y
+    [panel_x, pnael_y]
+  end
+
+  def initialize m, n
+    @m, @n = m, n
+    @direction_x, @direction_y = 1, -1
+
+    # 交点の配列
+    # 一番側に範囲外=-1を設定
+    @board = Array.new(n + 3) do |i_row|
+      if (1..(n + 1)).include? i_row
+        Array.new(m + 3) do |i_col|
+          ((1..(m + 1)).include? i_col) ? 0 : -1
+        end
+      else
+        Array.new(m + 3) {-1}
+      end
+    end
+    # Solver.display board
+  end
+
+  def move x, y
+    next_x, next_y = x + @direction_x, y + @direction_y
+
+    # 上下へ反射
+    return next_x, next_y if @board[next_y][next_x] > -1
+
+    if next_y == 0 || next_y == @board.length - 1
+      @direction_y = @direction_y * -1
+      next_x, next_y = next_x, next_y + @direction_y * 2
+    end
+
+    # 左右へ反射
+    if next_x == 0 || next_x == @board[0].length - 1
+      @direction_x = @direction_x * -1
+      next_x, next_y = next_x + @direction_x * 2, next_y
+    end
+
+    # p "#{@direction_x}, #{@direction_y}"
+    return next_x, next_y
+  end
+
+  def solve
+    route = []
+    passed = Set.new
+    tiles = []
+    (1..@m).each do |x_start|
+      (1..@n).each do |y_start|
+        x, y = x_start, y_start
+        while true do
+          # p "#{x}, #{y}"
+          x_prev, y_prev = x, y
+          x, y = move x, y
+          key = "#{x_prev},#{y_prev}->#{x},#{y}"
+          break if route.include? key
+          route << key
+          passed << Solver.panel(x_prev, y_prev, x, y)
+        end
+        tiles << passed.length 
+      end
+    end
+
+    [tiles.min, tiles.max]
   end
 end
 
 m, n = STDIN.gets.chomp.split(",").map{|v| v.to_i}
+solver = Solver.new m, n
 
-# 交点の配列
-# 一番側に範囲外=-1を設定
-board = Array.new(n + 3) do |i_row|
-  if (1..(n + 1)).include? i_row
-    Array.new(m + 3) do |i_col|
-      ((1..(m + 1)).include? i_col) ? 0 : -1
-    end
-  else
-    Array.new(m + 3) {-1}
-  end
-end
-# display board
-
-@direction_x, @direction_y = 1, -1
-
-def move board, x, y
-  next_x, next_y = x + @direction_x, y + @direction_y
-
-  # 上下へ反射
-  return next_x, next_y if board[next_y][next_x] > -1
-
-  if next_y == 0 || next_y == board.length - 1
-    @direction_y = @direction_y * -1
-    next_x, next_y = next_x, next_y + @direction_y * 2
-  end
-
-  # 左右へ反射
-  if next_x == 0 || next_x == board[0].length - 1
-    @direction_x = @direction_x * -1
-    next_x, next_y = next_x + @direction_x * 2, next_y
-  end
-
-  # p "#{@direction_x}, #{@direction_y}"
-  return next_x, next_y
-end
-
-def panel x_prev, y_prev, x, y
-  panel_x = x_prev < x ? x_prev : x
-  pnael_y = y_prev < y ? y_prev : y
-  [panel_x, pnael_y]
-end
-
-route = []
-passed = Set.new
-tiles = []
-(1..m).each do |x_start|
-  (1..n).each do |y_start|
-    x, y = x_start, y_start
-    while true do
-      # p "#{x}, #{y}"
-      x_prev, y_prev = x, y
-      x, y = move board, x, y
-      key = "#{x_prev},#{y_prev}->#{x},#{y}"
-      break if route.include? key
-      route << key
-      passed << panel(x_prev, y_prev, x, y)
-    end
-    tiles << passed.length 
-  end
-end
-
-p tiles.min, tiles.max
+solver.solve.each {|val| p val}
