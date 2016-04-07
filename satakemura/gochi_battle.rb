1a3ec9f526f03696125968edfe10bb0ced7f1278
Fukayanegi <fukayanegi1981@gmail.com>
2016-04-07 22:30:18 +0900

    implement: サンプル問題解答可能 #101

--- satakemura/gochi_battle.rb
+++ satakemura/gochi_battle.rb
@@ -1,8 +1,33 @@
-target = STDIN.gets.chomp
+target = STDIN.gets.chomp.to_i
 menu = []
 while line = STDIN.gets do
-  menu << line.chomp
+  menu << line.chomp.to_i
+end
+
+total = menu.inject(:+)
+surplus = total - target
+
+p "target: #{target}, total: #{total}, surplus: #{surplus}"
+p menu
+
+if target > surplus
+  target = surplus
 end
 
 p "target: #{target}"
-p menu
+
+min = target
+1.upto(menu.length) do |num|
+  diffs = []
+  menu.combination(num).each do |comb|
+    diffs << target - comb.inject(:+)
+  end
+
+  min_tmp = diffs.map{|v| v.abs}.min
+  min = min_tmp if min_tmp < min
+  break if min == 0
+  break if diffs.all?{|val| val < 0}
+  p "min: #{min}"
+end
+
+p min
