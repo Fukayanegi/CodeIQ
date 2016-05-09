ecdaba634c2233d0e686b2727d7cdc5a76b99f14
Fukayanegi <fukayanegi1981@gmail.com>
2016-05-10 00:23:09 +0900

    implement: 周囲の辺の長さnの格子において最短経路の場合の数を考える #115

--- question_of_this_week/width_of_shortest_round_trip_square.rb
+++ question_of_this_week/width_of_shortest_round_trip_square.rb
@@ -1,1 +1,19 @@
-N = STDIN.gets.chomp.to_i
+n = STDIN.gets.chomp.to_i
+
+def factorial(number)
+  number = 0 if number.nil?
+  (1..number).inject(1,:*)
+end
+
+answer = 0
+(1..(n/2)).each do |width|
+  height = n - width
+  routes = factorial(n) / (factorial(width) * factorial(n - width))
+  p "#{width}, #{routes}"
+  if routes.even?
+    answer = width
+    break
+  end
+end
+
+puts answer
