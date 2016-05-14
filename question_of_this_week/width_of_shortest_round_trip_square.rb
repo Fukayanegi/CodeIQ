5358dd76d0e24635b9f0f10697704cb1934245a4
Fukayanegi <fukayanegi1981@gmail.com>
2016-05-15 07:36:51 +0900

    improve: 場合の数を計算する際の階乗計算をメモ化、約分で効率化 #115

--- question_of_this_week/width_of_shortest_round_trip_square.rb
+++ question_of_this_week/width_of_shortest_round_trip_square.rb
@@ -1,15 +1,34 @@
 n = STDIN.gets.chomp.to_i
 
+@memo_f = Hash.new
 def factorial(number)
   number = 0 if number.nil?
-  (1..number).inject(1,:*)
+  return @memo_f[number] if @memo_f.include? number
+  @memo_f[number] = (1..number).inject(1,:*)
+  @memo_f[number]
+end
+
+@memo_fp = Hash.new
+def factorial_partial(from, to)
+  return to if from == to
+
+  key = "#{from}:#{to}"
+  return @memo_fp[key] if @memo_fp.include? key
+  @memo_fp[key] = factorial_partial(from + 1, to)
+  @memo_fp[key]
 end
 
 answer = 0
+width = n / 2
+# while width != 1 do
 (1..(n/2)).each do |width|
   height = n - width
-  routes = factorial(n) / (factorial(width) * factorial(n - width))
-  p "#{width}, #{routes}"
+  # routes = factorial(n) / (factorial(width) * factorial(n - width))
+  mother = factorial_partial(width + 1, n)
+  child = factorial(width)
+  routes = mother / child
+
+  p "#{mother}, #{child}, #{routes}"
   if routes.even?
     answer = width
     break
