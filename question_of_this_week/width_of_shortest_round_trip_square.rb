18c2c9d2c36d0ec0a4c38f0d58fb2c709852020c
Fukayanegi <fukayanegi1981@gmail.com>
2016-05-16 23:02:40 +0900

    improve: Nが奇数のときのみ重点的に考えればよいことに気づき性能改善 Closes #115

--- question_of_this_week/width_of_shortest_round_trip_square.rb
+++ question_of_this_week/width_of_shortest_round_trip_square.rb
@@ -14,25 +14,38 @@ def factorial_partial(from, to)
 
   key = "#{from}:#{to}"
   return @memo_fp[key] if @memo_fp.include? key
-  @memo_fp[key] = factorial_partial(from + 1, to)
+  @memo_fp[key] = from * factorial_partial(from + 1, to)
   @memo_fp[key]
 end
 
 answer = 0
-width = n / 2
-# while width != 1 do
-(1..(n/2)).each do |width|
+width = 1
+while width <= n / 2 do
+  # 最短距離の場合の数は、factorial(n) / (factorial(width) * factorial(n - width))
+  # この値が偶数になるケースはwidthの増分に対応する場合の数への乗数(height + 1) / widthが偶数になる場合のみ
+  # なぜなら1つ前のwidthでfactorial(n) / (factorial(width) * factorial(n - width))が奇数だった場合、
+  # 現在の(height + 1) / widthが奇数の場合は、奇数*奇数で奇数となる
+  # 現在の(height + 1) / widthが割り切れない場合、奇数*(height + 1)となるが、
+  # (height + 1)が偶数ということは、割り切れなかったwidthは奇数、heightも奇数だが、この場合
+  # width + heightが偶数となり、width = 1でfactorial(n) / (factorial(width) * factorial(n - width))が偶数になるはず
+  # なのでありえない
+
   height = n - width
   # routes = factorial(n) / (factorial(width) * factorial(n - width))
-  mother = factorial_partial(width + 1, n)
-  child = factorial(width)
-  routes = mother / child
+  # mother = factorial_partial(height + 1, n)
+  # child = factorial(width)
+  # routes = mother / child
+  routes = (height + 1) / width
+  mod = (n - width + 1) % width
 
-  p "#{mother}, #{child}, #{routes}"
-  if routes.even?
+  # p "#{width}, #{mother}, #{child}, #{routes}"
+  # p "#{width}, #{routes}, #{mod}"
+  # sleep(2)
+  if routes.even? && mod == 0
     answer = width
     break
   end
+  width += 1
 end
 
 puts answer
