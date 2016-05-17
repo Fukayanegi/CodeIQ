c79fceafdf76be20dec1874bef698ae9444b9243
Fukayanegi <fukayanegi1981@gmail.com>
2016-05-17 23:38:29 +0900

    fix: 出力をn進数にしていなかったためto_sで変換 Closes #118

--- question_of_this_week/narcissistic_number.rb
+++ question_of_this_week/narcissistic_number.rb
@@ -2,13 +2,15 @@ n = STDIN.gets.chomp.to_i
 cardinal = n
 
 num = "10".to_i(n)
+# p "init: #{num}"
 while n > 0 do
   num_c = num.to_s(cardinal)
   len = num_c.length
   num_rev = num_c.each_char.to_a.inject(0){|acm, val| acm += val.to_i**len; acm}
   # p "#{num_rev}"
   if num_rev == num
-    p "#{num_c}, #{num}"
+    # p "#{num_c}, #{num}"
+    puts num.to_s(cardinal)
     n -= 1
   end
   num += 1
