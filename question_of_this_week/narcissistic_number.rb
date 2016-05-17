ea2f349b6b0fade82180f10517c2da9278c6ea47
Fukayanegi <fukayanegi1981@gmail.com>
2016-05-17 23:33:10 +0900

    implement: 最初の実装。パフォーマンス未考慮 #118

---
+++ question_of_this_week/narcissistic_number.rb
@@ -0,0 +1,15 @@
+n = STDIN.gets.chomp.to_i
+cardinal = n
+
+num = "10".to_i(n)
+while n > 0 do
+  num_c = num.to_s(cardinal)
+  len = num_c.length
+  num_rev = num_c.each_char.to_a.inject(0){|acm, val| acm += val.to_i**len; acm}
+  # p "#{num_rev}"
+  if num_rev == num
+    p "#{num_c}, #{num}"
+    n -= 1
+  end
+  num += 1
+end
