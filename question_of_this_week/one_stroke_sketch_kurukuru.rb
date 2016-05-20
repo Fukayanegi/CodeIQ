d6b0c889b6b9f1df2dc21d26f5dd273ec8e1d1bd
Fukayanegi <fukayanegi1981@gmail.com>
2016-05-21 06:12:10 +0900

    implement: 一筆書きの条件判定のため経路情報を記録しながら移動する #121

--- question_of_this_week/one_stroke_sketch_kurukuru.rb
+++ question_of_this_week/one_stroke_sketch_kurukuru.rb
@@ -5,12 +5,20 @@ COL = 4
 GOAL = [ROW,COL]
 
 n = STDIN.gets.chomp.to_i
-
-map = [4][5]
-
 @path = Set.new
+
+def record_path_and_move pos, direction, n
+  pos_next = [pos[0] + direction[0], pos[1] + direction[1]]
+  path = [pos, pos_next]
+  @path << path
+  answer = move pos_next, direction, n
+  @path.delete path
+
+  answer
+end
+
 def move pos, direction, n
-  p "call: #{pos}, #{direction}, #{n}, #{@path.to_a}"
+  # p "call: #{pos}, #{direction}, #{n}, #{@path.to_a}"
   # sleep 1
   return 0 if n < 0 && pos != GOAL
   return 0 if pos[0] < 0 || pos[1] < 0
@@ -18,12 +26,13 @@ def move pos, direction, n
   return 1 if pos == GOAL && n == 0
   sum = 0
   if n >= 0
-    # sum += move [pos[0] - 1, pos[1]], [-1, 0], (direction == [-1, 0] ? n - 1: n)
-    @path << pos
-    sum += move [pos[0], pos[1] + 1], [0, 1], (direction == [0, 1] ? n : n - 1)
-    sum += move [pos[0] + 1, pos[1]], [1, 0], (direction == [1, 0] ? n : n - 1)
-    # sum += move [pos[0], pos[1] - 1], [1, -1], (direction == [0, - 1] ? n - 1: n)
-    @path.delete pos
+    # @path << [pos, [pos[0] - 1, pos[1]]]
+    # sum += move [pos[0] - 1, pos[1]], [-1, 0], (direction == [-1, 0] ? n : n - 1)
+    # @path.delete [pos, [pos[0] - 1, pos[1]]]
+    sum += record_path_and_move pos, [0, 1], (direction == [0, 1] ? n : n - 1)
+    sum += record_path_and_move pos, [1, 0], (direction == [1, 0] ? n : n - 1)
+    # sum += move [pos[0], pos[1] - 1], [1, -1], (direction == [0, - 1] ? n : n - 1)
+    # @path.delete pos
   end
   sum
 end
