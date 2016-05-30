89a698a13ab70d68bc82999d2a4f0aea45d7af3f
Fukayanegi <fukayanegi1981@gmail.com>
2016-05-30 23:53:07 +0900

    improve: 細かい関数の呼び出し回数を減らすことにより全体の処理時間を減らすことに成功 Closes #121

--- question_of_this_week/one_stroke_sketch_kurukuru.rb
+++ question_of_this_week/one_stroke_sketch_kurukuru.rb
@@ -3,6 +3,9 @@ require 'set'
 ROW = 3
 COL = 4
 GOAL = [ROW,COL]
+FILTER = {[-1,0]=>11, [0,1]=>7, [1,0]=>14, [0,-1]=>13}
+BLOCK_FROM = {[-1,0]=>1, [0,1]=>2, [1,0]=>4, [0,-1]=>8}
+BLOCK_TO = {[-1,0]=>4, [0,1]=>8, [1,0]=>1, [0,-1]=>2}
 @vertex = [[9,1,1,1,3], [8,0,0,0,2], [8,0,0,0,2], [12,4,4,4,6]]
 
 n = STDIN.gets.chomp.to_i
@@ -29,83 +32,48 @@ def check_vertex pos
   cant_use
 end
 
-def filter direction
-  f = 11 if direction == [-1, 0]
-  f = 7 if direction == [0, 1]
-  f = 14 if direction == [1, 0]
-  f = 13 if direction == [0, -1]
-  f
-end
-
-def block_from direction
-  b = 1 if direction == [-1, 0]
-  b = 2 if direction == [0, 1]
-  b = 4 if direction == [1, 0]
-  b = 8 if direction == [0, -1]
-  b
-end
-
-def block_to direction
-  b = 4 if direction == [-1, 0]
-  b = 8 if direction == [0, 1]
-  b = 1 if direction == [1, 0]
-  b = 2 if direction == [0, -1]
-  b
-end
-
 def record_path_and_move pos, direction, n, passed
   if passed == 0
     g_from_x = (n.even? && direction == [1, 0]) || (n.odd? && direction == [0, 1]) ? [ROW, COL-1] : [ROW-1, COL]
     g_direction_x = (n.even? && direction == [1, 0]) || (n.odd? && direction == [0, 1]) ? [0, 1] : [1, 0]
-    @vertex[g_from_x[0]][g_from_x[1]] = @vertex[g_from_x[0]][g_from_x[1]] | block_from(g_direction_x)
-    @vertex[ROW][COL] = @vertex[ROW][COL] | block_to(g_direction_x)
-    # p g_from_x
-    # p g_direction_x
+    @vertex[g_from_x[0]][g_from_x[1]] |= BLOCK_FROM[g_direction_x]
+    @vertex[ROW][COL] |= BLOCK_TO[g_direction_x]
   end
-  # p @vertex if passed == 0
   @calls += 1
   pos_next = [pos[0] + direction[0], pos[1] + direction[1]]
-
-  # p "#{pos}, #{pos_next}, #{direction}, #{n}" if pos == [3, 3] && direction == [0, 1]
-  # p "#{pos}, #{pos_next}, #{direction}, #{n}, #{@path}" if pos_next == GOAL && n == 0
-  # p "#{pos}, #{pos_next}, #{direction}, #{n}, #{@path}" if pos == [3,2] && pos_next == [3,3] && n == 3
-  # p @vertex if pos == [3,2] && pos_next == [3,3] && n == 3
-  # p @vertex[pos_next[0]][pos_next[1]] if pos == [3,2] && pos_next == [3,3] && n == 3
-  # p filter(direction) if pos == [3,2] && pos_next == [3,3] && n == 3
+  pos_x, pos_y = pos[0], pos[1]
+  pos_next_x, pos_next_y = pos_next[0], pos_next[1]
 
   return 0 if n < 0 && pos_next != GOAL
-  return 0 if pos_next[0] < 0 || pos_next[1] < 0
-  return 0 if pos_next[0] > ROW || pos_next[1] > COL
-  return 0 if (@vertex[pos_next[0]][pos_next[1]] | filter(direction) == 15) && pos_next != GOAL
+  return 0 if pos_next_x < 0 || pos_next_y < 0
+  return 0 if pos_next_x > ROW || pos_next_y > COL
+  return 0 if (@vertex[pos_next_x][pos_next_y] | FILTER[direction] == 15) && pos_next != GOAL
   return 0 if (ROW * (COL+1) + COL * (ROW+1)) - passed < n + 1
-  return 0 if @vertex[pos[0]][pos[1]] & block_from(direction) != 0
+  return 0 if @vertex[pos_x][pos_y] & BLOCK_FROM[direction] != 0
   return 0 if @vertex[ROW][COL] == 15
   return 1 if pos_next == GOAL && n == 0
 
-  @vertex[pos[0]][pos[1]] = @vertex[pos[0]][pos[1]] | block_from(direction)
-  @vertex[pos_next[0]][pos_next[1]] = @vertex[pos_next[0]][pos_next[1]] | block_to(direction)
+  @vertex[pos_x][pos_y] |=  BLOCK_FROM[direction]
+  @vertex[pos_next_x][pos_next_y] |= BLOCK_TO[direction]
 
   cant_use = (check_vertex pos)
   cant_use.each do |k, v|
-    @vertex[k[0]][k[1]] = @vertex[k[0]][k[1]] | v
+    @vertex[k[0]][k[1]] |= v
   end
 
-  # tmp = Marshal.load(Marshal.dump(@vertex))
-@path << "#{pos}->#{pos_next}"
   answer = move pos_next, direction, n, passed
-@path.delete "#{pos}->#{pos_next}"
 
-  @vertex[pos[0]][pos[1]] = @vertex[pos[0]][pos[1]] & (15 - block_from(direction))
-  @vertex[pos_next[0]][pos_next[1]] = @vertex[pos_next[0]][pos_next[1]] & (15 - block_to(direction))
+  @vertex[pos_x][pos_y] &= (15 - BLOCK_FROM[direction])
+  @vertex[pos_next_x][pos_next_y] &= (15 - BLOCK_TO[direction])
   cant_use.each do |k, v|
-    @vertex[k[0]][k[1]] = @vertex[k[0]][k[1]] & (15 - v)
+    @vertex[k[0]][k[1]] &= (15 - v)
   end
 
   if passed == 0
     g_from_x = (n.even? && direction == [1, 0]) || (n.odd? && direction == [0, 1]) ? [ROW, COL-1] : [ROW-1, COL]
     g_direction_x = (n.even? && direction == [1, 0]) || (n.odd? && direction == [0, 1]) ? [0, 1] : [1, 0]
-    @vertex[g_from_x[0]][g_from_x[1]] = @vertex[g_from_x[0]][g_from_x[1]] & (15 - block_from(g_direction_x))
-    @vertex[ROW][COL] = @vertex[ROW][COL] & (15 - block_to(g_direction_x))
+    @vertex[g_from_x[0]][g_from_x[1]] &= (15 - BLOCK_FROM[g_direction_x])
+    @vertex[ROW][COL] &= (15 - BLOCK_TO[g_direction_x])
   end
   answer
 end
@@ -115,25 +83,14 @@ def move pos, direction, n, passed
   # sleep 1
   sum = 0
   if n >= 0
-    sum += record_path_and_move pos, [-1, 0], (direction == [-1, 0] ? n : n - 1), passed+1 if direction != [1, 0]
-    sum += record_path_and_move pos, [0, 1], (direction == [0, 1] ? n : n - 1), passed+1 if direction != [0, -1]
-    sum += record_path_and_move pos, [1, 0], (direction == [1, 0] ? n : n - 1), passed+1 if direction != [-1, 0]
-    sum += record_path_and_move pos, [0, -1], (direction == [0, -1] ? n : n - 1), passed+1 if direction != [0, 1]
+    sum += record_path_and_move pos, direction, n, passed + 1
+    sum += record_path_and_move pos, [-1*direction[1], -1*direction[0]], n - 1, passed + 1
+    sum += record_path_and_move pos, [direction[1], direction[0]], n - 1, passed + 1
   end
   sum
 end
 
-# p "*"*40
 answer1, answer2 = 0, 0
-# パフォーマンス未解決
-# if n == 21
-#   puts 6
-# elsif n == 22
-#   puts 40
-# else
-  answer1 = record_path_and_move [0,0], [0,1], n, 0
-  answer2 = record_path_and_move [0,0], [1,0], n, 0
-  puts answer1 + answer2
-  # puts "answer1: #{answer1}, answer2: #{answer2}"
-  puts @calls
-# end
+answer1 = record_path_and_move [0,0], [0,1], n, 0
+answer2 = record_path_and_move [0,0], [1,0], n, 0
+puts answer1 + answer2
