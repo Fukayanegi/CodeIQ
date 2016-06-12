c25989cda0cafb97e86992d19bef29ae094e2b41
Fukayanegi <fukayanegi1981@gmail.com>
2016-06-12 23:06:09 +0900

    fix: 重複組み合わせを考慮していなかったバグを修正 #131
    fix: statusハッシュへの解析で2要素以上保持できていなかったバグを修正 #131

--- satakemura/the_strongest_deck.rb
+++ satakemura/the_strongest_deck.rb
@@ -12,14 +12,15 @@ costs = STDIN.gets.chomp.to_i
 status = {}
 while line = STDIN.gets do
   cost, score = line.chomp.split(" ").map{|v| v.to_i}
-  status[cost] = [] if !status.include? score
+  status[cost] = [] if !status.include? cost
   status[cost] << score
 end
 
 status.each do |cost, cards_by_cost|
-  p cards_by_cost
+  # p cards_by_cost
   cards_by_cost.sort!{|a, b| b <=> a}
 end
+# p status
 
 # p "cards, #{cards}"
 # p "costs, #{costs}"
@@ -28,8 +29,9 @@ end
 # カードの組み合わせを考える
 patterns = []
 (2..cards).each do |num_of_cards|
-  patterns.concat status.keys.combination(num_of_cards).select{|comb| comb.inject(:+) < costs}
+  patterns.concat status.keys.repeated_combination(num_of_cards).select{|comb| comb.inject(:+) < costs}
 end
+# p patterns
 
 # 組み合わせごとに、同一コストの中から最大のスコアを持つカードを選択する
 score_tmp = 0
@@ -37,6 +39,11 @@ score_max = 0
 patterns.each do |pattern|
   counts = pattern.count
   counts.each do |cost, count|
+    if status[cost].length < count
+      # そのコストのカードが足りない場合は組み合わせとして考えられないためスキップ
+      score_tmp = 0
+      next
+    end
     score_tmp += status[cost].first(count).inject(:+)
   end
   score_max = score_tmp if score_tmp > score_max
