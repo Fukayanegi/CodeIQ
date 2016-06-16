77dfb9c340e91e100666b3f85c75ea06bf838094
Fukayanegi <fukayanegi1981@gmail.com>
2016-06-16 23:24:27 +0900

    fix: 解にmを含む実装となっていた不具合を修正 #137
    fix: nの10進数表記中に重複する数値がある場合の考慮が漏れていた不具合を修正 #137

--- nabetani/the_exclusive_number_of_power_of_n.rb
+++ nabetani/the_exclusive_number_of_power_of_n.rb
@@ -1,11 +1,15 @@
 n, m = STDIN.gets.chomp.split(",").map{|v| v.to_i}
 
 answer = "-"
-n.downto(0) do |num|
-  num_p = num.to_s.each_char.to_a.uniq
-  num_of_power_p = (num**m).to_s.each_char.to_a.uniq
+(n-1).downto(0) do |num|
+  pattern_of_num = num.to_s.each_char.to_a.uniq
+  pattern_of_num_of_power = (num**m).to_s.each_char.to_a.uniq
 
-  if num_p & num_of_power_p == []
+  # p "#{num}, #{num**m}, #{pattern_of_num}, #{pattern_of_num_of_power}"
+
+  next if pattern_of_num.length != pattern_of_num.length
+
+  if pattern_of_num & pattern_of_num_of_power == []
     answer = num
     break
   end
