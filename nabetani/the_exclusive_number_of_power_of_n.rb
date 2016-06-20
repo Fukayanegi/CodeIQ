212af2e30b0a2d9aa261b1d7748053b4a9e1d816
Fukayanegi <fukayanegi1981@gmail.com>
2016-06-20 23:23:15 +0900

    improve: 検査対象を狭めることで効率改善 Closes #137

--- nabetani/the_exclusive_number_of_power_of_n.rb
+++ nabetani/the_exclusive_number_of_power_of_n.rb
@@ -1,13 +1,16 @@
 n, m = STDIN.gets.chomp.split(",").map{|v| v.to_i}
+digit = n.to_s.length
+
+target = ("0".."9").to_a.permutation(digit).map{|num_a| num_a.join.to_i}.select{|num| num < n}
 
 answer = "-"
-(n-1).downto(0) do |num|
+target.sort{|a, b| b <=> a}.each do |num|
   pattern_of_num = num.to_s.each_char.to_a.uniq
+  next if num.to_s.length != pattern_of_num.length
+  
   pattern_of_num_of_power = (num**m).to_s.each_char.to_a.uniq
 
   # p "#{num}, #{num**m}, #{pattern_of_num}, #{pattern_of_num_of_power}"
-
-  next if num.to_s.length != pattern_of_num.length
 
   if pattern_of_num & pattern_of_num_of_power == []
     answer = num
