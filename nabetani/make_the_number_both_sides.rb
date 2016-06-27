0b7f6c8ada4a29f3a45d5194da090969ffd1d190
Fukayanegi <fukayanegi1981@gmail.com>
2016-06-27 23:04:28 +0900

    implement:最初の実装。いろいろバグあり #143

---
+++ nabetani/make_the_number_both_sides.rb
@@ -0,0 +1,36 @@
+input = STDIN.gets.chomp.split(",")
+num_of_cards = input[0].to_i
+target = input[1].to_i
+cards = input[2].split("/").map{|pair| pair.each_char.to_a.map{|num| num.to_i}}
+
+# p "#{num_of_cards}, #{target}, #{cards}"
+
+if cards.select{|v| v[0] || v[1]}.length == 0
+  puts "-"
+else
+  pattern = cards.permutation(num_of_cards)
+  abs = pattern.inject({}) do |h, num|
+    key = Array.new(num_of_cards){0}
+    sides = Array.new(num_of_cards){0}
+    num_of_cards.times do |i_card|
+      key[i_card] = num[i_card][0]
+      table = (target - key.join.to_i).abs
+      key[i_card] = num[i_card][1]
+      back = (target - key.join.to_i).abs
+      
+      side = table < back ? 0 : 1
+      side = 1 if num[i_card][0] == 0
+      side = 0 if num[i_card][1] == 0
+      key[i_card] = num[i_card][side]
+      sides[i_card] = side
+    end
+
+    num_abs_min = key.join.to_i
+    h[num_abs_min] = (target - num_abs_min).abs if !(key[0] == 0 && num_abs_min != 0)
+    h
+  end
+  # p abs.values.min
+
+  min = abs.values.min
+  puts abs.select{|key, value| key if value == min}.keys.join(",")
+end
