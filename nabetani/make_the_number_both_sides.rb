33e4a7bf608d0b854d8c5d79d595b2e83b68d81c
Fukayanegi <fukayanegi1981@gmail.com>
2016-07-03 22:49:39 +0900

    implement: 中間結果の保持方法から文字列を排除することで高速化 #143

--- nabetani/make_the_number_both_sides.rb
+++ nabetani/make_the_number_both_sides.rb
@@ -8,23 +8,53 @@ cards = input[2].split("/").map{|pair| pair.each_char.to_a.map{|num| num.to_i}}
 if cards.select{|v| v[0] != 0 || v[1] != 0}.length == 0
   puts "-"
 else
-  pattern = cards.permutation(num_of_cards)
-  abs = pattern.inject({}) do |h, num|
-    key = Array.new(num_of_cards){0}
-    num_of_cards.times do |i_card|
-      key[i_card] = num[i_card][0]
-      table = (target - key.join.to_i).abs
-      key[i_card] = num[i_card][1]
-      back = (target - key.join.to_i).abs
-      
-      side = table < back ? 0 : 1
-      side = 1 if num[i_card][0] == 0 && num_of_cards > 1 && i_card == 0
-      side = 0 if num[i_card][1] == 0 && num_of_cards > 1 && i_card == 0
-      key[i_card] = num[i_card][side]
-    end
+  perm = cards.permutation(num_of_cards)
+  tmp_num_abs_min = ("9"*num_of_cards).to_i
 
-    num_abs_min = key.join.to_i
-    h[num_abs_min] = (target - num_abs_min).abs if !(key[0] == 0 && num_abs_min != 0)
+  abs = perm.inject({}) do |h, num|
+    flg = false
+    tmp_num, tmp_num_abs = 0, target
+    table, back = 0, 0
+    table_abs, back_abs = target, target
+
+    num_of_cards.times do |i_card|
+      # カードごとにどちら側の数字を使うかを上位桁から判定
+      # To Fix: 下記のケースが作れるカードの組の場合、解けない
+      # target = 2345, table = 3346, back = 1344
+
+      side = -1
+      digit = 10 ** (num_of_cards - 1 - i_card)
+      adjust = target % digit
+      # p "#{digit}, #{num[i_card]}"
+      if num[i_card][0] == 0 && num_of_cards > 1 && i_card == 0
+        side = 1
+        back = num[i_card][side] * digit + adjust
+        back_abs = (target - back).abs
+      elsif num[i_card][1] == 0 && num_of_cards > 1 && i_card == 0
+        side = 0
+        table = num[i_card][side] * digit + adjust
+        table_abs = (target - table).abs
+      else
+        tmp = (0..1).map{|i| num[i_card][i] * digit + adjust}
+        table, back = tmp_num + tmp[0], tmp_num + tmp[1]
+        
+        table_abs, back_abs = (target - table).abs, (target - back).abs
+        side = table_abs < back_abs ? 0 : 1
+      end
+
+      tmp_num = (side == 0) ? table - adjust : back - adjust
+      tmp_num_abs = (side == 0) ? table_abs : back_abs
+      # p "#{tmp_num}, #{tmp_num_abs}, #{i_card}, #{table}, #{back}"
+
+      if tmp_num_abs > tmp_num_abs_min
+        flg = true
+        break
+      end
+    end
+    next h if flg
+
+    num_abs_min = tmp_num
+    h[tmp_num] = tmp_num_abs
     h
   end
   # p abs.values.min
