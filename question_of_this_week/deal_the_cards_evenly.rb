78009ddf8f7c26efd87964624c000794013e3bbd
Fukayanegi <fukayanegi1981@gmail.com>
2016-07-05 00:00:42 +0900

    improve: 困った時のビット計算で高速化 Closes #145

--- question_of_this_week/deal_the_cards_evenly.rb
+++ question_of_this_week/deal_the_cards_evenly.rb
@@ -1,22 +1,35 @@
-m, n = STDIN.gets.chomp.split(",").map{|v| v.to_i}
+@m, @n = STDIN.gets.chomp.split(",").map{|v| v.to_i}
 
-s = (1 + m) * m / 2
+@nums = (1..@m).to_a
+@s = (1 + @m) * @m / 2
+# p s/n
 
-if s % n != 0 
+@memo = {}
+def seach select, patterns, i, rest
+  return 1 if rest == 0
+  answer = 0
+  i.upto(patterns.length - 1) do |j|
+    # p "#{select}, #{patterns}, #{patterns[j]}, #{i}, #{rest}"
+    next if select & patterns[j] != 0
+    answer += seach(select | patterns[j], patterns, j+1, rest-1)
+  end
+  answer
+end
+
+if @s % @n != 0 
   # 明らかに解がないケース
   puts 0
 else
-  # 1からmの数字でnになるパターン
-  cards = (1..m).to_a
+  # 1からmの数字でs/nになるパターン
+  cards = (1..@m).to_a
   patterns = []
-  (1.upto m).each do |num_of_cards|
+  (1.upto @m).each do |num_of_cards|
     ps = cards.combination(num_of_cards).select do |comb|
-      comb.inject(:+) == s / n
+      comb.inject(:+) == @s / @n
     end
-    patterns.concat ps
+
+    patterns.concat ps.map{|p| p.inject(0){|acm, this| acm += 1 << (this-1)}}
   end
-  answers = patterns.combination(n).select do |comb|
-    comb.flatten.sort == cards
-  end
-  puts answers.length
+
+  puts seach 0, patterns, 0, @n
 end
