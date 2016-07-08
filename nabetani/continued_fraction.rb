5a6015215a0304696f883e413bc0ac865dcc96db
Fukayanegi <fukayanegi1981@gmail.com>
2016-07-08 23:54:44 +0900

    refactor: コメント付与 Closes #147

--- nabetani/continued_fraction.rb
+++ nabetani/continued_fraction.rb
@@ -1,13 +1,17 @@
 input = STDIN.gets.chomp
 nums = input.split("+").map{|num| num[1..-2].split(";")}.map{|num| [num[0].to_i, num[1].split(",").map{|v| v.to_i}]}
 
+# 分数部分を抽出
 fraction_a, fraction_b = nums.map do |num|
   x, y = 1, 0
   num[1].reverse.each{|v| x, y = y + x * v, x;}
   [y, x]
 end
 
+# 通分して足し算した分母(x)、分子(y)
 y, x = fraction_a[0] * fraction_b[1] + fraction_b[0] *  fraction_a[1], fraction_a[1] * fraction_b[1]
+
+# ユークリッド互除法適用
 denominator = []
 while y != 0 do
   # p "#{x}, #{y}"
@@ -15,6 +19,7 @@ while y != 0 do
   x, y = y, x % y
 end
 
+# x == y の場合は整数部分の足し算に1を足すだけ
 if denominator == [1]
   puts "#{nums[0][0] + nums[1][0] + 1}"
 else
