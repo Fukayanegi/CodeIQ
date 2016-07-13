b209aefdf215bee6709531ea100f7f3a24af4a6f
Fukayanegi <fukayanegi1981@gmail.com>
2016-07-13 18:10:38 +0900

    fix: 入力に2桁の数字があると読み取れない不具合を修正 Closes #151

--- nabetani/here_is_liars.rb
+++ nabetani/here_is_liars.rb
@@ -1,21 +1,27 @@
 # 入力値の処理
 talks = STDIN.gets.chomp.split(",").map do |talk|
-  t = talk.scan(/(.*):(.*)(\d)/)[0]
+  t = talk.scan(/([a-zA-Z]*):([a-zA-Z]*)(\d*)/)[0]
   [t[0], t[1].each_char.to_a, t[2].to_i]
 end
 kyura_max = talks.each.inject(0){|acc, talk| acc = talk[2] if acc < talk[2]; acc}
 persons = talks.map{|talk| talk[0]}
-# p persons
+# p "#{kyura_max}, #{persons.join}"
 
 answer = []
+# キュラ族のパターンごとに、下記の判定
+# キュラ族の場合、発言が真
+# ラゲ族の場合、発言が偽
 (0..kyura_max).each do |kyuras_num|
-  persons.to_a.combination(kyuras_num).each do |kyuras|
+  persons.combination(kyuras_num).each do |kyuras|
+    # p "start: #{kyuras.join}"
     judge = true
     talks.each do |talk|
       r = talk[1].inject(0){|acc, person| acc = acc + 1 if (kyuras.include? person); acc}
       talk_judge = kyuras.include? talk[0]
+
+      # キュラ族の発言が偽、ラゲ族の発言が真の場合、このキュラ族のパターンではない
       if (r == talk[2].to_i) != talk_judge
-        # p "#{kyuras}, #{talk_judge}, #{(r == talk[2])}, #{r}, #{talk}"
+        # p "missmatch: #{kyuras.join}, #{talk_judge}, #{(r == talk[2])}, #{r}"
         judge = false
         break
       end
