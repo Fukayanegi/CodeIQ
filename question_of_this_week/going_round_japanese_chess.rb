a4a3a6634cb7342b51065fc9a21eb530cff574aa
Fukayanegi <fukayanegi1981@gmail.com>
2016-07-23 01:40:42 +0900

    improve: 組み合わせの数を元に順列を数え上げる作戦で高速化。でもなんか難易度の割にこねくり回している感じがする。 Closes #163

--- question_of_this_week/going_round_japanese_chess.rb
+++ question_of_this_week/going_round_japanese_chess.rb
@@ -20,22 +20,29 @@ patterns = calc_steps STEPS, 4, true
 # p patterns.length
 
 # 角からはみ出す数ごとのパターン数
-patterns_by_out_steps = {0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 7 => 0}
-patterns.inject(patterns_by_out_steps) do |acc, v|
+patterns_by_out_steps = patterns.inject({}) do |acc, v|
   key = v % 8
-  acc[key] += 1
+  (acc.include? key) ? (acc[key] += 1) : (acc[key] = 1)
   acc
 end
 # p patterns_by_out_steps
 
-# target = n回振ってはみ出す数の和が8で割り切れる数になる順列
-target_comb = (0..7).to_a.repeated_permutation(n).select{|comb| comb.inject(:+) % 8 == 0}
+# target_comb = n回振ってはみ出す数の和が8で割り切れる数になる組み合わせ
+# ここで順列を求めると計算時間が膨大になってしまう
+target_comb = (0..7).to_a.repeated_combination(n).select{|comb| comb.inject(:+) % 8 == 0}
 # p target_comb
 
 # target毎に各はみ出す数のパターン数を乗算したものの和が答え
 answer = target_comb.inject(0) do |acc, comb|
-  acc += comb.inject(1) do |acc_inner, v|
+  # 組み合わせのパターンから順列を計算
+  factor = (1..n).to_a.inject(:*)
+  comb.inject({}){|acc, v| (acc.include? v) ? (acc[v] += 1) : (acc[v] = 1); acc}.each do |k, v|
+    factor /= (1..v).to_a.inject(:*)
+  end
+  tmp = comb.inject(1) do |acc_inner, v|
     acc_inner *= patterns_by_out_steps[v]
   end
+
+  acc += tmp * factor
 end
 p answer
