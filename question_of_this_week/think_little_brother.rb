4c669dd8b7178e1e6c4c7e29a425446edfe40142
Fukayanegi <fukayanegi1981@gmail.com>
2016-07-22 23:27:29 +0900

    improve: メモ化により高速化 Closes #161

--- question_of_this_week/think_little_brother.rb
+++ question_of_this_week/think_little_brother.rb
@@ -4,8 +4,11 @@ rows = params[1]
 brothers = params[2]
 # p "#{cols}, #{rows}, #{brothers}"
 
+@memo = {}
 def solve cols, rows, brothers
-  return 1 if brothers == 0
+  key = "#{cols}:#{rows}:#{brothers}"
+  return cols - 1 + rows - 1 if brothers <= 2
+  return @memo[key] if @memo.include? key
   patterns = 0
 
   (1..cols-1).each do |col|
@@ -16,7 +19,8 @@ def solve cols, rows, brothers
     patterns += solve cols, rows - row, brothers - 1
   end
 
+  @memo[key] = patterns
   patterns
 end
 
-puts (solve cols, rows, brothers - 1)
+puts (solve cols, rows, brothers)
