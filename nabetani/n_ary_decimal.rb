364af9ccfe5d489b5bce624479af73983a4e1f26
Fukayanegi <fukayanegi1981@gmail.com>
2016-08-01 06:17:58 +0900

    fix : 小数部の開始が0の場合桁がずれてしまう不具合を修正。これで解けたと思われるが。。。 Closes #167

--- nabetani/n_ary_decimal.rb
+++ nabetani/n_ary_decimal.rb
@@ -9,22 +9,31 @@ def split_nrd num
 end
 
 def plus_nrd num1, num2
-
   num1_n, num1_str = split_nrd num1
   num2_n, num2_str = split_nrd num2
-  answer = []
+  answer_decimal = []
 
   # 小数部の計算
   rise = 0
   (0..8).each do |idx|
-    p "#{idx}, #{num1_str[idx]}, #{num1_str}"
-    p "#{idx}, #{num2_str[idx]}, #{num2_str}"
+    tmp = num1_str[idx].to_i + num2_str[idx].to_i + rise
+    rise = tmp / (2 + idx)
+    remainder = tmp % (2 + idx)
+    answer_decimal.unshift remainder
   end
 
-  # 整数部の計算
-  p num1_n + num2_n + rise
+  # 整数部の計算に小数部を結合
+  ((num1_n + num2_n + rise).to_s + "." + answer_decimal.join).to_f
 end
 
-input_strs.each do |line_no, exp, answer|
-  plus_nrd *(exp.split("+").map{|v| v.to_f})
+wrong_line = []
+input_strs.each do |line_no, exp, answer_input|
+  answer = plus_nrd *(exp.split("+").map{|v| v.to_f})
+  if answer != answer_input.to_f
+    # デバッグ用
+    # p "#{line_no}, #{exp}, #{answer_input} : #{answer}"
+    wrong_line << line_no
+  end
 end
+
+puts wrong_line.join(",")
