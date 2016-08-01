013fbca133ed15a4d7d681925bb23b5f695c44be
Fukayanegi <fukayanegi1981@gmail.com>
2016-08-02 08:42:55 +0900

    implement: 最初の実装 #171

---
+++ nabetani/judge_sequence.rb
@@ -0,0 +1,56 @@
+sequence = STDIN.gets.chomp.split(" ").map{|v| v.to_i}
+
+@fibonacci = [1, 1]
+@limit = 10000000000
+num1 = @fibonacci[0]
+num2 = @fibonacci[1]
+while num1 + num2 <= @limit
+  @fibonacci << num1 + num2
+  num1, num2 = num2, num1 + num2
+end
+
+def make_sq_cd first, second
+  diff = second - first
+  sq = []
+
+  5.times do |t|
+    sq << first + t * diff
+  end
+  sq
+end
+
+def make_sq_gmtc first, second
+  ratio = second / first
+  sq = []
+  return sq if second % first != 0
+
+  5.times do |t|
+    sq << first * ratio ** t
+  end
+  sq
+end
+
+def make_sq_fb first, second
+  idx = @fibonacci.find{|num| num == first} - 1
+  sq = []
+  return sq if idx.nil?
+  idx += 1 if second != @fibonacci[idx+1]
+
+  sq = @fibonacci[idx..idx+4]
+  sq
+end
+
+first, second = sequence[0..1]
+sq1 = make_sq_cd first, second
+sq2 = make_sq_gmtc first, second
+sq3 = make_sq_fb first, second
+
+if sequence == sq1
+  puts "A"
+elsif sequence == sq2
+  puts "G"
+elsif sequence == sq3
+  puts "F"
+else
+  puts "x"
+end
