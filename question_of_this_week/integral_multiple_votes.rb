c5432cddbbf41d3803a0807b7dd282a0f9a0f96b
Fukayanegi <fukayanegi1981@gmail.com>
2016-08-02 00:42:01 +0900

    fix: 最低得票以外のパターン数を求める際の基数を1にしていなかった不具合を修正 Closes #169

--- question_of_this_week/integral_multiple_votes.rb
+++ question_of_this_week/integral_multiple_votes.rb
@@ -16,10 +16,13 @@ end
 
 answer = 0
 (1..min_votes_limit).each do |min_votes|
+  # p "minvotes: #{min_votes}"
   if n % min_votes == 0
     rest = n / min_votes - 1
+    # p "rest: #{rest}"
     if rest >= m - 1
-      tmp = patterns rest, m - 1, min_votes
+      tmp = patterns rest, m - 1, 1
+      # p "#{rest}, #{m-1}, #{min_votes}: #{tmp}" if tmp > 0
       answer += tmp
     end
   end
