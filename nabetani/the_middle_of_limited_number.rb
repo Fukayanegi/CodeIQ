7d4af820dfde4098e8eaf2602c0088a7d6f5a934
Fukayanegi <fukayanegi1981@gmail.com>
2016-08-09 23:59:47 +0900

    refactor: 解が２つあるケースの対応時の制御を上位に持って行くことにより単純化 Closes #173

--- nabetani/the_middle_of_limited_number.rb
+++ nabetani/the_middle_of_limited_number.rb
@@ -17,67 +17,31 @@ targets = total_count.even? ? [total_count / 2 - 1, total_count / 2] : [total_co
 # p @count_by_digits
 # p "#{total_count}, #{targets}"
 
-def solve_under_digits digits, numbers, target, highest_zero
-  # p "call solve_under_digits #{digits}, #{numbers}, #{target}"
+def solve digits, numbers, target, highest_zero
+  # p "call solve #{digits}, #{numbers}, #{target}"
   return "" if digits < 1
   return numbers.each_char.sort[target] if digits == 1
 
   is_amp = @including_zero && numbers.length > 1 && digits > 2
   count = is_amp ? @count_by_digits[digits - 1] / (numbers.length - 1) * numbers.length : @count_by_digits[digits - 1]
-  # p "digits = #{digits - 1}, count_by_digits = #{@count_by_digits[digits - 1]}, count = #{count}"
   idx = target / count
-  # p "idx = #{idx}"
 
   highest_digit = numbers.each_char.select{|c| highest_zero || c != "0"}.sort[idx]
-  repeat_digit = numbers.each_char.sort[0]
-
   rest_target = target - count * idx
-  rest_target = count - 1 if rest_target < 0
-
-  highest_digit + (solve_under_digits digits - 1, numbers, rest_target, true)
+  highest_digit + (solve digits - 1, numbers, rest_target, true)
 end
 
-def solve digits, numbers, targets
-  # p "call solve #{digits}, #{numbers}, #{targets}"
-  answer = []
-
-  if digits == 1
-    targets.each do |target|
-      answer << numbers.each_char.to_a[target]
-    end
-    return answer
-  end
-
+answer = []
+targets.each do |target|
   total_count = 0
-
-  1.upto(digits) do |i_digits|
+  digits = 0
+  while total_count <= target
+    digits += 1
     prev = total_count
-    total_count += @count_by_digits[i_digits]
-    next if total_count <= targets[0]
-    # i_digits桁に解がある
-    # p "digits = #{digits}, i_digits = #{i_digits}, target = #{targets}, total = #{total_count}"
-
-    # 2つ目の解がちょうど桁変わりだった場合
-    # 直接求めてもよい
-    if total_count == targets[0] + 1 && targets.length > 1
-      answer.concat (solve i_digits + 1, numbers, [targets[1]])
-      # p "2nd_answer: #{answer}"
-      targets.pop
-    end
-
-    targets.length.times do |i_rest|
-      rest_count = targets[0] + i_rest - prev
-
-      if i_digits == 1
-        answer << (solve_under_digits 1, numbers, rest_count, true)
-      else
-        answer << (solve_under_digits i_digits, numbers, rest_count, false)
-      end
-    end
-
-    break
+    total_count += @count_by_digits[digits]
+    rest_count = target - prev
   end
-  answer.map{|num| num.to_i}.sort.map{|num| num.to_s}
+  answer << (solve digits, numbers, rest_count, false)
 end
 
-puts (solve limit_digits.to_i, numbers, targets).join(",")
+puts answer.join(",")
