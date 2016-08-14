ee10f53055cf69461569839724afab92aa6656bb
Fukayanegi <fukayanegi1981@gmail.com>
2016-08-15 06:12:23 +0900

    fix: むきになっていろいろ修正。主に[1,2,2,3]みたいなとき[2,2]で発生するグループ間の重複を考慮を修正。 #175

--- question_of_this_week/grouping_by_entry_number.rb
+++ question_of_this_week/grouping_by_entry_number.rb
@@ -1,40 +1,92 @@
+def factorial n
+  return 1 if n < 1
+  (1..n).to_a.inject{|acc, v| acc * v}
+end
+
+def combination n, r
+  permutation(n,r) / factorial(r)
+end
+
+def permutation n, r
+  factorial(n) / factorial(n - r)
+end
+
 n = STDIN.gets.chomp.to_i
 
+@memo = {}
 def calc_e_value m
   # 全員が座席番号=申込番号のケース
   patterns = 1
   groups_sum = m
 
   # 座席番号と申込番号が等しい人数
-  (0..m-1).each do |num_of_same|
-    num_of_same_patterns = num_of_same == 0 ? 1 : (1..m).to_a.combination(num_of_same).to_a.length
+  (0..m-2).each do |num_of_same|
+    num_of_same_patterns = num_of_same == 0 ? 1 : combination(m, num_of_same)
     rest = m - num_of_same
     max_groups = rest / 2
 
-    # p "#{num_of_same}:start, groups_sum = #{groups_sum}, patterns = #{patterns}"
+    # p "m = #{m}, #{num_of_same}:start"
+    # p "groups_sum = #{groups_sum}, patterns = #{patterns}, num_of_same_patterns = #{num_of_same_patterns}"
 
-    (1..max_groups).each do |groups|
-      # p "num_of_same = #{num_of_same}, rest = #{rest}, groups = #{groups}"
-      (1..rest).to_a.combination(groups - 1).each do |cut_point|
-        menbers = cut_point.unshift(0).push(rest).each_cons(2).map{|t, n| n - t}
+    count_m = {}
+    if @memo.include? rest
+      count_m = @memo[rest]
+      patterns += num_of_same_patterns * count_m.values.inject(0, :+)
+      groups_sum += num_of_same_patterns * count_m.inject(0){|acc, (key, value)| acc += (key + num_of_same) * value}
+      # p "patterns = #{num_of_same_patterns * count_m.values.inject(0, :+)}, groups_sum = #{num_of_same_patterns * count_m.inject(0){|acc, (key, value)| acc += (key + num_of_same) * value}}"
+    else
+      # 分割するグループ数
+      (1..max_groups).each do |groups|
+        # p "groups = #{groups}, rest = #{rest}"
+        # グループ数が同一の場合、[2,3]、[3,2]のようにカットポイントが異なっても同一のグループ分けになる場合がある
+        memo = []
+        (1..rest).to_a.combination(groups - 1).each do |cut_point|
+          members = cut_point.unshift(0).push(rest).each_cons(2).map{|t, n| n - t}.sort
 
-        # 残りrest個の数値に対してcut_pointは有効か？
-        if menbers.all?{|member| member > 1}
-          # 座席番号と申し込み番号が等しい人数の組み合わせ数を初期値に
-          count = num_of_same == 0 ? (1..m-1).inject(1){|acc, this| acc * (m - this)} : num_of_same_patterns
+          # 残りrest個の数値に対してcut_pointは有効か？
+          if members.all?{|member| member > 1} && !(memo.include? members)
+            memo << members
+            # p "groups = #{groups}, members = #{members}, num_of_same = #{num_of_same}"
 
-          # restをmenbers毎に分割していく組み合わせ数を乗算
-          menbers.inject(rest) do |acc, this|
-            # TODO: 場合の数でlengthを求める
-            count *= (1..acc).to_a.combination(this).to_a.length
-            # p "acc = #{acc}, this = #{this}, count = #{count}"
-            acc - this
+            # restをmembers毎に分割していく組み合わせ数を乗算
+            count = 1
+            members.inject(rest) do |acc, group|
+              # restからgroupのグループを作成できる組み合わせ数
+              # p "rest = #{acc}, group = #{group}"
+              cm = combination(acc, group)
+              # p "combination(rest, group) = #{cm}"
+              # group内の辿り順の場合の数
+              # 先頭を固定する必要があるため、n = group - 1とする
+              pm = permutation(group - 1, group - 1)
+              # p "permutation(group - 1, group - 1) = #{pm}"
+              tmp = cm * pm
+              # p "tmp = #{tmp}"
+              count *= tmp
+              acc - group
+            end
+            
+            # [[12], [34], [56]],[[43], [12], [56]]等の重複分を除算
+            dup_comb = members.inject({}) do |acc_h, num_of_group|
+              acc_h[num_of_group] = 0 if !(acc_h.include? num_of_group)
+              acc_h[num_of_group] += 1
+              acc_h
+            end
+            dup_comb = dup_comb.select{|k, v| v > 1}
+            # p "dup_comb = #{dup_comb}"
+            div_dup_comb = dup_comb.inject(1){|acm_d, (k, v)| acm_d * factorial(v)}
+            # p "count = #{count}, div_dup_comb = #{div_dup_comb}"
+            count = count / div_dup_comb
+
+            patterns += num_of_same_patterns * count
+            count_m[groups] = 0 if !(count_m.include? groups)
+            count_m[groups] += count
+            groups_sum += num_of_same_patterns * (groups + num_of_same) * count
+            # p "patterns = #{num_of_same_patterns * count}, groups_sum = #{num_of_same_patterns * (groups + num_of_same) * count}"
           end
-          patterns += count
-          groups_sum += (menbers.length + num_of_same) * count
-          # p "count = #{count} menbers = #{menbers}, groups_sum = #{groups_sum}"
         end
       end
+
+      # @memo[rest] = count_m
     end
   end
 
