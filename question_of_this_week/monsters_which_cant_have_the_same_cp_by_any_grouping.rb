32ff799400f25dfe0e8f9ee827b78d158aaed292
Fukayanegi <fukayanegi1981@gmail.com>
2016-08-20 05:57:14 +0900

    implement: パターンを安直に求めるも時間がかかり過ぎる #177

--- question_of_this_week/monsters_which_cant_have_the_same_cp_by_any_grouping.rb
+++ question_of_this_week/monsters_which_cant_have_the_same_cp_by_any_grouping.rb
@@ -1,2 +1,17 @@
 cp, n = STDIN.gets.chomp!.split(" ").map{|v| v.to_i}
 p "#{cp}, #{n}"
+
+@memo = Hash.new(n)
+def solve_1 cp_l, cp_h, n
+  p "call solve: cp_l = #{cp_l}, cp_h = #{cp_h}, n = #{n}"
+  return if n == 0
+
+  (cp_l..cp_h-n).each do |cp|
+    n.times do |i|
+      @memo[i] += cp
+    end
+    solve cp + 1, cp_h, n - 1
+  end
+end
+
+p solve 1, cp, n
