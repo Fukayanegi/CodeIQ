d873ef52fc51b9e3a31b79723a64f07311d2c127
Fukayanegi <fukayanegi1981@gmail.com>
2016-08-25 05:48:18 +0900

    fix: height > widthの場合の条件の不具合を修正 #179

--- kawazoe/multiple_table.rb
+++ kawazoe/multiple_table.rb
@@ -9,25 +9,25 @@ answer = 0
     base_width_next = start_r * start_c + start_r * (width - 1)
     while base_width_next <= n
       base_width = base_width_next
-      width += 1
-      base_width_next += start_r * start_c + start_r * (width - 1)
-
       height = 1
       sum = 0
-      base = base_width / start_r
-      sum_next = base_width + base * (height - 1)
-      while sum_next <= n && height <= start_c + width - 1
+      delta = base_width / start_r
+      sum_next = base_width + delta * (height - 1)
+      while (sum_next <= n) && (start_c != start_r || height <= width)
         sum = sum_next
         height += 1
-        sum_next += base_width + base * (height - 1)
+        sum_next += base_width + delta * (height - 1)
       end
       height -= 1
 
-      # p "start_r = #{start_r}, start_c = #{start_c}, width = #{width - 1}, base_width = #{base_width}" if sum == n
+      # p "start_r = #{start_r}, start_c = #{start_c}, width = #{width}, base_width = #{base_width}, delta = #{delta}" if sum == n
       # p "height = #{height}, sum = #{sum}" if sum == n
-      # p "single" if sum == n && start_r == start_c && width - 1 == height
+      # p "single" if sum == n && start_r == start_c && width == height
       answer += 1 if sum == n
-      answer += 1 if sum == n && (start_r != start_c || width - 1 != height)
+      answer += 1 if sum == n && (start_r != start_c || width != height)
+
+      width += 1
+      base_width_next += start_r * start_c + start_r * (width - 1)
     end
   end
 end
