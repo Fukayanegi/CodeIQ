d849aedd0575d9c015000104567e1601f086c352
Fukayanegi <fukayanegi1981@gmail.com>
2016-08-27 10:48:09 +0900

    improve: 終了条件を早めることで高速化 Closes #179

--- kawazoe/multiple_table.rb
+++ kawazoe/multiple_table.rb
@@ -1,33 +1,30 @@
 n = STDIN.gets.chomp.to_i
 
 answer = 0
-1.upto(n) do |start_r|
-  start_r.upto(n) do |start_c|
-    # 1行目の数値
-    width = 1
-    base_width = 0
-    base_width_next = start_r * start_c + start_r * (width - 1)
-    while base_width_next <= n
-      base_width = base_width_next
-      height = 1
-      sum = 0
-      delta = base_width / start_r
-      sum_next = base_width + delta * (height - 1)
-      while (sum_next <= n) && (start_c != start_r || height <= width)
-        sum = sum_next
-        height += 1
-        sum_next += base_width + delta * (height - 1)
-      end
-      height -= 1
+1.upto(Math.sqrt(n).to_i) do |start_r|
+  start_r.upto(n / start_r) do |start_c|
+    # 1行のみで解はあるか
+    b = 2 * start_c - start_r
+    s = (-1 * b + Math.sqrt(b ** 2 + 8 * start_r * n)) / (2 * start_r)
+    # p "start_r = #{start_r}, start_c = #{start_c}, solution = #{s}"
 
-      # p "start_r = #{start_r}, start_c = #{start_c}, width = #{width}, base_width = #{base_width}, delta = #{delta}" if sum == n
-      # p "height = #{height}, sum = #{sum}" if sum == n
-      # p "single" if sum == n && start_r == start_c && width == height
-      answer += 1 if sum == n
-      answer += 1 if sum == n && (start_r != start_c || width != height)
+    width = 1
+    base = 0
+    base_next = start_r * start_c + start_r * (width - 1)
+    while base_next <= n
+      base = base_next
+      delta = base / start_r
+      b_h = 2 * base - delta
+      height = (-1 * b_h + Math.sqrt(b_h ** 2 + 8 * delta * n)) / (2 * delta)
+      # p "start_r = #{start_r}, start_c = #{start_c}, width = #{width}, solution = #{height}" if height % 1 == 0
+
+      if !(start_r == start_c && height.to_i > width)
+        answer += 1 if height % 1 == 0
+        answer += 1 if (height % 1 == 0) && !(start_r == start_c && width == height.to_i)
+      end
 
       width += 1
-      base_width_next += start_r * start_c + start_r * (width - 1)
+      base_next += start_r * start_c + start_r * (width - 1)
     end
   end
 end
