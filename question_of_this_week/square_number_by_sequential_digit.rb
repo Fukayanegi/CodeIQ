2859ce35e6b734654f3cef5aa3bbd3785d82849e
Fukayanegi <fukayanegi1981@gmail.com>
2016-08-29 05:49:23 +0900

    implement: 最初の実装 #181

--- question_of_this_week/square_number_by_sequential_digit.rb
+++ question_of_this_week/square_number_by_sequential_digit.rb
@@ -22,35 +22,44 @@ answer = 0
 # 因数に2桁の数字がある時点で答えは0
 puts answer if primes_1_9 == []
 
-# 2,3,5,7のうちそれぞれ1つ以下の数字で構成されている場合、
-# その構成で作成可能な順列の数が有効
-constitution = primes_1_9.inject(Hash.new(0)){|acc, v| acc[v] = 0 if !acc.include? v; acc[v] += 1; acc}
+constitution = primes_1_9.inject(Hash.new{0}){|acc, v| acc[v] = 0 if !acc.include? v; acc[v] += 1; acc}
 p constitution
-answer += primes_1_9.permutation(primes_1_9.length).to_a.length if constitution.all?{|k, v| v <= 1}
+# 一つの因数のみで構成されている平方数
+answer += 1 if constitution.length == 1 && constitution.first[1] == 2
 
 #TODO: 以下のロジックをひとつにまとめられそうな気がする
 
-# 因数に2,3がある場合6を作成し、残った数字の構成に2,3が同時に含まれていなければ
-# 6と残った数字の構成で作成可能な順列の数が有効
-primes_1_9_tmp = primes_1_9.dup
-if constitution[2] == 1 && constitution[3] == 1
-  primes_1_9_tmp.delete 2
-  primes_1_9_tmp.delete 3
-  primes_1_9_tmp << 6
-  answer += primes_1_9_tmp.permutation(primes_1_9_tmp.length).to_a.length if constitution.all?{|k, v| v <= 1}
-end
-p primes_1_9_tmp
-
-# 因数に2が3つ以上ある場合8を作成し、残った数字の構成がそれぞれ1つ以下で構成されている場合
-# 8と残った数字の構成で作成可能な順列の数が有効
-primes_1_9_tmp = primes_1_9.dup
+# 因数に2が3つ以上ある場合8を作成
+# 作成した数字と残った数字の構成で作成可能な順列の数が有効
+# 残った数字に2,3がある場合（6が作成可能な場合）は6を先に作成した場合に考慮される
 if constitution[2] >= 3
-  primes_1_9_tmp.delete 2
-  (constitution[2] - 3).times{primes_1_9_tmp << 2}
+  n_tmp = n / 8
+  primes_1_9_tmp = factorize_prime_1_9 n_tmp
+  constitution_tmp = primes_1_9_tmp.inject({}){|acc, v| acc[v] = 0 if !acc.include? v; acc[v] += 1; acc}
+  p constitution_tmp
+
   primes_1_9_tmp << 8
-  constitution_tmp = primes_1_9_tmp.inject(Hash.new(0)){|acc, v| acc[v] = 0 if !acc.include? v; acc[v] += 1; acc}
+  p primes_1_9_tmp
+
   answer += primes_1_9_tmp.permutation(primes_1_9_tmp.length).to_a.length if constitution_tmp.all?{|k, v| v <= 1}
 end
-p primes_1_9_tmp
+
+# 因数に2,3がある場合6を作成
+# 残った数字に2が3つある場合8を作成
+# 6を作成した後は2が1つか、2が3つ以上となるが、2が3つ以上となる場合は8を作成しないと222の数列が存在しNGになる
+# 作成した数字と残った数字の構成で作成可能な順列の数が有効
+if (constitution.include? 2) && (constitution.include? 3)
+  n_tmp = n / 6
+  n_tmp = n_tmp / 8 if constitution[2] >= 4
+  primes_1_9_tmp = factorize_prime_1_9 n_tmp
+  constitution_tmp = primes_1_9_tmp.inject({}){|acc, v| acc[v] = 0 if !acc.include? v; acc[v] += 1; acc}
+  p constitution_tmp
+
+  primes_1_9_tmp << 6
+  primes_1_9_tmp << 8 if constitution[2] >= 4
+  p primes_1_9_tmp
+
+  answer += primes_1_9_tmp.permutation(primes_1_9_tmp.length).to_a.length if constitution_tmp.all?{|k, v| v <= 1}
+end
 
 puts answer
