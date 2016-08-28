fcd50419a65adc49d400a3e77a717c2c97ac2cb6
Fukayanegi <fukayanegi1981@gmail.com>
2016-08-29 06:11:39 +0900

    refactor: 重複部分を関数にまとめ上げ #181

--- question_of_this_week/square_number_by_sequential_digit.rb
+++ question_of_this_week/square_number_by_sequential_digit.rb
@@ -12,6 +12,15 @@ def factorize_prime_1_9 num
   []
 end
 
+def multiply primes, divs
+  sum = primes.inject(:*)
+  divs.each do |div|
+    sum /= div
+  end
+  primes_1_9_tmp = factorize_prime_1_9 sum
+  divs.concat primes_1_9_tmp
+end
+
 n = STDIN.gets.chomp.to_i
 
 # 1桁の数字で素因数分解
@@ -27,20 +36,12 @@ p constitution
 # 一つの因数のみで構成されている平方数
 answer += 1 if constitution.length == 1 && constitution.first[1] == 2
 
-#TODO: 以下のロジックをひとつにまとめられそうな気がする
-
 # 因数に2が3つ以上ある場合8を作成
 # 作成した数字と残った数字の構成で作成可能な順列の数が有効
 # 残った数字に2,3がある場合（6が作成可能な場合）は6を先に作成した場合に考慮される
 if constitution[2] >= 3
-  n_tmp = n / 8
-  primes_1_9_tmp = factorize_prime_1_9 n_tmp
+  primes_1_9_tmp = multiply primes_1_9, [8]
   constitution_tmp = primes_1_9_tmp.inject({}){|acc, v| acc[v] = 0 if !acc.include? v; acc[v] += 1; acc}
-  p constitution_tmp
-
-  primes_1_9_tmp << 8
-  p primes_1_9_tmp
-
   answer += primes_1_9_tmp.permutation(primes_1_9_tmp.length).to_a.length if constitution_tmp.all?{|k, v| v <= 1}
 end
 
@@ -49,16 +50,14 @@ end
 # 6を作成した後は2が1つか、2が3つ以上となるが、2が3つ以上となる場合は8を作成しないと222の数列が存在しNGになる
 # 作成した数字と残った数字の構成で作成可能な順列の数が有効
 if (constitution.include? 2) && (constitution.include? 3)
-  n_tmp = n / 6
-  n_tmp = n_tmp / 8 if constitution[2] >= 4
-  primes_1_9_tmp = factorize_prime_1_9 n_tmp
+  primes_1_9_tmp = []
+  if constitution[2] >= 4
+    primes_1_9_tmp = multiply primes_1_9, [6, 8]
+  else
+    primes_1_9_tmp = multiply primes_1_9, [6]
+  end
+
   constitution_tmp = primes_1_9_tmp.inject({}){|acc, v| acc[v] = 0 if !acc.include? v; acc[v] += 1; acc}
-  p constitution_tmp
-
-  primes_1_9_tmp << 6
-  primes_1_9_tmp << 8 if constitution[2] >= 4
-  p primes_1_9_tmp
-
   answer += primes_1_9_tmp.permutation(primes_1_9_tmp.length).to_a.length if constitution_tmp.all?{|k, v| v <= 1}
 end
 
