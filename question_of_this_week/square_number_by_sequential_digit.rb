3eb1c06b3ce3b2789aa6e6c224f378674bdf728c
Fukayanegi <fukayanegi1981@gmail.com>
2016-08-31 05:52:31 +0900

    stop: 時間切れのため開発中止 Closes #181

--- question_of_this_week/square_number_by_sequential_digit.rb
+++ question_of_this_week/square_number_by_sequential_digit.rb
@@ -18,47 +18,77 @@ def multiply primes, divs
     sum /= div
   end
   primes_1_9_tmp = factorize_prime_1_9 sum
-  divs.concat primes_1_9_tmp
+  answer = divs.concat primes_1_9_tmp
+  # p answer
+  answer
+end
+
+def is_valid sequence, additional
+  # p "call is_valid: sequence = #{sequence}, additional = #{additional}"
+  count = {additional => 1}
+  answer = true
+  (sequence.length).times do |i|
+    num = sequence[sequence.length - 1 + i]
+    count[num] = 0 if !count.include? num
+    count[num] += 1
+    if count.all?{|k, v| v.even?} && (i != sequence.length - 1)
+      answer = false
+      break
+    end
+  end
+
+  # p "sequence = #{sequence}, additional = #{additional}, answer = #{answer}"
+  answer
+end
+
+def count_patterns sequence, candidate_h
+  candidate = candidate_h.select{|k, v| v > 0}.keys
+  return 1 if candidate.length == 0
+  answer = 0
+  candidate.each do |k|
+    if is_valid sequence, k
+      candidate_h[k] -= 1
+      sequence << k
+      answer += count_patterns sequence, candidate_h
+      sequence.pop
+      candidate_h[k] += 1
+    end
+  end
+  answer
 end
 
 n = STDIN.gets.chomp.to_i
-
-# 1桁の数字で素因数分解
-# 因数に2桁の数字がある場合空の配列が返る
-primes_1_9 = factorize_prime_1_9 n
-
 answer = 0
-# 因数に2桁の数字がある時点で答えは0
-puts answer if primes_1_9 == []
 
-constitution = primes_1_9.inject(Hash.new{0}){|acc, v| acc[v] = 0 if !acc.include? v; acc[v] += 1; acc}
-p constitution
-# 一つの因数のみで構成されている平方数
-answer += 1 if constitution.length == 1 && constitution.first[1] == 2
+if n > 0
+  # 1桁の数字で素因数分解
+  # 因数に2桁の数字がある場合空の配列が返る
+  primes_1_9 = factorize_prime_1_9 n
 
-# 因数に2が3つ以上ある場合8を作成
-# 作成した数字と残った数字の構成で作成可能な順列の数が有効
-# 残った数字に2,3がある場合（6が作成可能な場合）は6を先に作成した場合に考慮される
-if constitution[2] >= 3
-  primes_1_9_tmp = multiply primes_1_9, [8]
-  constitution_tmp = primes_1_9_tmp.inject({}){|acc, v| acc[v] = 0 if !acc.include? v; acc[v] += 1; acc}
-  answer += primes_1_9_tmp.permutation(primes_1_9_tmp.length).to_a.length if constitution_tmp.all?{|k, v| v <= 1}
-end
+  # 因数に2桁の数字がある時点で答えは0
+  if primes_1_9 != []
+    constitution = primes_1_9.inject(Hash.new{0}){|acc, v| acc[v] = 0 if !acc.include? v; acc[v] += 1; acc}
+    # p constitution
+    # 一つの因数のみで構成されている平方数
+    answer += count_patterns [], constitution
+    # p "1: #{answer}"
 
-# 因数に2,3がある場合6を作成
-# 残った数字に2が3つある場合8を作成
-# 6を作成した後は2が1つか、2が3つ以上となるが、2が3つ以上となる場合は8を作成しないと222の数列が存在しNGになる
-# 作成した数字と残った数字の構成で作成可能な順列の数が有効
-if (constitution.include? 2) && (constitution.include? 3)
-  primes_1_9_tmp = []
-  if constitution[2] >= 4
-    primes_1_9_tmp = multiply primes_1_9, [6, 8]
-  else
-    primes_1_9_tmp = multiply primes_1_9, [6]
+    constitution[3].times do |num|
+      if constitution[2] >= num + 1
+        primes_1_9_tmp = multiply primes_1_9, [6]*(num + 1)
+        constitution_tmp = primes_1_9_tmp.inject({}){|acc, v| acc[v] = 0 if !acc.include? v; acc[v] += 1; acc}
+        answer += count_patterns [], constitution_tmp
+      end
+    end
+    # p "2: #{answer}"
+
+    (constitution[2] / 3).times do |num|
+      primes_1_9_tmp = multiply primes_1_9, [8]*(num + 1)
+      constitution_tmp = primes_1_9_tmp.inject({}){|acc, v| acc[v] = 0 if !acc.include? v; acc[v] += 1; acc}
+      answer += count_patterns [], constitution_tmp
+    end
+    # p "3: #{answer}"
   end
-
-  constitution_tmp = primes_1_9_tmp.inject({}){|acc, v| acc[v] = 0 if !acc.include? v; acc[v] += 1; acc}
-  answer += primes_1_9_tmp.permutation(primes_1_9_tmp.length).to_a.length if constitution_tmp.all?{|k, v| v <= 1}
 end
 
 puts answer
