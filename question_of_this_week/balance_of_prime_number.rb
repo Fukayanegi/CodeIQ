8355e8208cff081ecd0c84acf196ad4a6302005a
Fukayanegi <fukayanegi1981@gmail.com>
2016-09-08 11:41:53 +0900

    implement: m以下の素数配列を作成するコードを実装 #190

---
+++ question_of_this_week/balance_of_prime_number.rb
@@ -0,0 +1,29 @@
+m, n = STDIN.gets.chomp.split(" ").map{|v| v.to_i}
+
+# 素数配列作成
+@primes = []
+@limit = 1
+def make_primes limit
+  ((@limit+1)..limit).each do |num|
+    is_prime = true
+    quotient = limit
+    @primes.each do |prime|
+      # 判定対象が前回判定時の商を超える素数だった場合、
+      # 以降の商は前回の素数を下回る（既に存在しないことが判定済）ため打ち切り
+      if prime > quotient
+        is_prime = true
+        break
+      end
+      quotient = num / prime
+      if num % prime == 0
+        is_prime = false
+        break
+      end
+    end
+    @primes << num if is_prime
+  end
+  @limit = limit
+end
+
+make_primes m
+p @primes
