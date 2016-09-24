55b6007f65612f0488fa7b5c829577c16d598e18
Fukayanegi <fukayanegi1981@gmail.com>
2016-09-25 05:42:46 +0900

    improve: 桁長オーバー対策として余りのみを計算。高速化対応として余りのループを検知 Closes #198

--- kawazoe/triangle_maze.rb
+++ kawazoe/triangle_maze.rb
@@ -1,24 +1,28 @@
-test_case = [100, 213, 214, 1170, 87654321, 100000000]
-
 class Solver
   @@dividend = 1000003
   attr_accessor :n
 
   def initialize n
     @n = n
-  end
-
-  def solve_inner n
-    answer = 1
-    (n - 1).times do
-      answer = answer + answer**2
-    end
-    answer
+    @memo = []
   end
 
   def solve
-    answer = solve_inner @n
-    answer % @@dividend
+    answer = 1
+    (@n - 1).times do |time|
+      if @memo.include? answer
+        loop_start = @memo.find_index answer
+        loop_end = @memo.length
+        loop_rest = @memo[loop_start..loop_end-1]
+        loop_len = loop_end - loop_start
+        answer = loop_rest[(@n - time) % loop_len - 1]
+        # p "time:#{time}, answer:#{answer}, @memo:#{loop_rest[0]}, @memo:#{loop_rest[(@n - time) % loop_len - 1]}"
+        break
+      end
+      @memo << answer
+      answer = (answer * (answer + 1)) % @@dividend
+    end
+    answer
   end
 end
 
