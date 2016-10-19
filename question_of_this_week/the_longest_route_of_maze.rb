8745eaa5340d8c03db07172ed0423e708fd9ae52
Fukayanegi <fukayanegi1981@gmail.com>
2016-10-20 06:09:41 +0900

    refactor: メッソッド抽出、不要コメントの削除 Closes #212

--- question_of_this_week/the_longest_route_of_maze.rb
+++ question_of_this_week/the_longest_route_of_maze.rb
@@ -13,6 +13,14 @@ class Board
     @board_org = @board
   end
 
+  def bit_length
+    (width + 2) * (height + 2)
+  end
+
+  def mask row, col
+    1 << (bit_length - 1 - (row * (width + 2) + col))
+  end
+
   def show
     board.to_s(2).scan(/.{1,#{width + 2}}/).each do |line|
       puts line
@@ -20,37 +28,32 @@ class Board
   end
 
   def fill cell
-    base = (width + 2) * (height + 2)
     cell -= 1
     row = cell / width + 1
     col = cell % width + 1
-    self.board = (@board | (1 << (base - row * (width + 2) - col - 1)))
+    self.board = (@board | mask(row, col))
   end
 
   def clear
     self.board = @board_org
   end
 
-  def blank? w, h
-    base = (width + 2) * (height + 2)
-    i = base - 1 - (h * (width + 2) + w)
-    board & (1 << i) == 0
-    # ("%0#{base}b" % self.board)[i].to_i
+  def blank? row, col
+    (board & mask(row, col)) == 0
   end
 
   def go_to_goal step_strategy
     answer = 1
-    direction, w, h = DIRECTION[:DOWN], 1, 1
-    while !(w == width && h == height)
-      # p "w: #{w}, h: #{h}, direction: #{direction}"
-      direction, w_next, h_next = step_strategy.step(direction, w, h, self)
-      answer += 1 if w_next != w || h_next != h
-      if direction == DIRECTION[:LEFT] && w_next == 1 && h_next == 1
+    direction, row, col = DIRECTION[:DOWN], 1, 1
+    while !(row == width && col == height)
+      direction, row_next, col_next = step_strategy.step(direction, row, col, self)
+      answer += 1 if row_next != row || col_next != col
+      if direction == DIRECTION[:LEFT] && row_next == 1 && col_next == 1
+        # スタート地点で左向きになっていた場合ゴールに辿り着けずに戻ってきたことになるので解なし
         answer = -1
         break
       end
-      w, h = w_next, h_next
-      # sleep(1)
+      row, col = row_next, col_next
     end
     answer
   end
@@ -58,34 +61,31 @@ end
 
 class RightHandStrategy
   def step direction, w, h, board
+    # 方向転換もしくは右手側へ進む
     case direction
     when DIRECTION[:DOWN] then
-      # p "w: #{w - 1}, h: #{h}, blank?: #{board.blank?(w - 1, h)}"
-      if board.blank?(w - 1, h)
+      if board.blank?(h, w - 1)
         w -= 1
         direction = DIRECTION[:LEFT]
       else
         direction = DIRECTION[:RIGHT]
       end
     when DIRECTION[:RIGHT] then
-      # p "w: #{w}, h: #{h + 1}, blank?: #{board.blank?(w, h + 1)}"
-      if board.blank?(w, h + 1)
+      if board.blank?(h + 1, w)
         h += 1
         direction = DIRECTION[:DOWN]
       else
         direction = DIRECTION[:UP]
       end
     when DIRECTION[:UP] then
-      # p "w: #{w + 1}, h: #{h}, blank?: #{board.blank?(w + 1, h)}"
-      if board.blank?(w + 1, h)
+      if board.blank?(h, w + 1)
         w += 1
         direction = DIRECTION[:RIGHT]
       else
         direction = DIRECTION[:LEFT]
       end
     when DIRECTION[:LEFT] then
-      # p "w: #{w}, h: #{h - 1}, blank?: #{board.blank?(w, h - 1)}"
-      if board.blank?(w, h - 1)
+      if board.blank?(h - 1, w)
         h -= 1
         direction = DIRECTION[:UP]
       else
