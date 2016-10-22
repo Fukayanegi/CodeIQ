b064027c871ff7bdfc2a11048d5141676394d4ba
Fukayanegi <fukayanegi1981@gmail.com>
2016-10-22 21:51:16 +0900

    fix: よくわからないけどリファクタしたらバグが直ってた Closes #214

--- nabetani/rational_number_by_both_sides.rb
+++ nabetani/rational_number_by_both_sides.rb
@@ -14,13 +14,13 @@ def parent_rational rational
 end
 
 def left_child rational
-  p "#{rational}'s left child: #{Rational(rational.numerator, rational.numerator + rational.denominator)}"
+  # p "#{rational}'s left child: #{Rational(rational.numerator, rational.numerator + rational.denominator)}"
   Rational(rational.numerator, rational.numerator + rational.denominator)
 end
 
 def right_child rational
   return nil if rational.numerator * 2 >= rational.denominator
-  p "#{rational}'s right child: #{Rational(rational.denominator - rational.numerator, rational.denominator)}"
+  # p "#{rational}'s right child: #{Rational(rational.denominator - rational.numerator, rational.denominator)}"
   Rational(rational.denominator - rational.numerator, rational.denominator)
 end
 
@@ -28,9 +28,9 @@ def left rational
   return nil if rational.numerator == 1
 
   parent = parent_rational(rational)
-  p "#{rational}'s parent: #{parent}"
+  # p "#{rational}'s parent: #{parent}"
   if is_left_child?(rational)
-    p "#{rational}' is left_child"
+    # p "#{rational}' is left_child"
     parent_left = left(parent)
     if parent_left.nil?
       ancestor = parent_rational(parent)
@@ -38,12 +38,12 @@ def left rational
       parent_left = left_child(ansestor_left)
     end
     tmp = right_child(parent_left) || left_child(parent_left)
-    p "#{rational}'s left; #{tmp}"
+    # p "#{rational}'s left; #{tmp}"
     tmp
   else
-    p "#{rational}' is right_child"
+    # p "#{rational}' is right_child"
     tmp = left_child(parent)
-    p "#{rational}'s left; #{tmp}"
+    # p "#{rational}'s left; #{tmp}"
     tmp
   end
 end
@@ -52,28 +52,27 @@ def right rational
   return nil if rational.denominator <= 3
 
   parent = parent_rational(rational)
-  p "#{rational}'s parent: #{parent}"
+  # p "#{rational}'s parent: #{parent}"
   parent_right = right(parent)
-  # p "#{parent}'s right: #{parent_right}"
 
   if is_left_child?(rational)
-    p "#{rational}' is left_child"
+    # p "#{rational}' is left_child"
     tmp = right_child(parent) || (parent_right.nil? ? nil : left_child(parent_right))
-    p "#{rational}'s right; #{tmp}"
+    # p "#{rational}'s right; #{tmp}"
     tmp
   else
-    p "#{rational}' is right_child"
+    # p "#{rational}' is right_child"
     if parent_right.nil?
       ansestor = parent_rational(parent)
       ansestor_right = right(ansestor)
       parent_right = ansestor_right.nil? ? nil : left_child(ansestor_right)
     end
     tmp = parent_right.nil? ? nil : left_child(parent_right)
-    p "#{rational}'s right; #{tmp}"
+    # p "#{rational}'s right; #{tmp}"
     tmp
   end
 end
 
-p left(target)
-p right(target)
+# p left(target)
+# p right(target)
 puts "%s,%s" % [left(target) || "-", right(target) || "-"]
