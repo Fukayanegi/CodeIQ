95f4c77f3c65102a32ba6490246328b621a145f2
Fukayanegi <fukayanegi1981@gmail.com>
2016-10-24 05:40:58 +0900

    implement: observeパターンを目指す。cellの場所を誰が管理するかに悩む #214

--- nabetani/monotonic_increase_on_hex.rb
+++ nabetani/monotonic_increase_on_hex.rb
@@ -1,8 +1,12 @@
 class Hex
+  @@neighbor_pos = [:LEFT_ABOVE, :RIGHT_AVOVE, :LEFT, :RIGHT, :LEFT_BENEATH, :RIGHT_BENEATH]
+
   class Cell
     include Comparable
-    attr_accessor :value, :longest_path
-    def initialize char
+    attr_accessor :row, :col, :value, :longest_path
+    def initialize row, col, char
+      @row = row
+      @col = col
       @value = char.ord
       @longest_path = 1
     end
@@ -15,7 +19,7 @@ class Hex
       @value - other.value
     end
 
-    def recieve_signal neighbor_longest_path
+    def update_longest_path neighbor_longest_path
       if longest_path < 1 + neighbor_longest_path
         self.longest_path = 1 + neighbor_longest_path 
         true
@@ -27,7 +31,14 @@ class Hex
 
   attr_accessor :hex
   def initialize str
-    @hex = str.split("/").map{|line| line.each_char.map{|c| Cell.new(c)}}
+    @hex = []
+    str.split("/").each_with_index do |line, row|
+      h_line = []
+      line.each_char.each_with_index do |c, col|
+        h_line << Cell.new(row, col, c)
+      end
+      hex << h_line
+    end
   end
 
   def show
@@ -42,6 +53,23 @@ class Hex
     end
   end
 
+  def search_longest_path
+    hex.each_with_index do |line, row|
+      line.each_with_index do |cell , col|
+        notify(row, col)
+      end
+    end
+  end
+
+  def longest_path
+    hex.inject(0) do |acc, line|
+      line.inject(acc) do |acc_sub, cell| 
+        acc_sub = acc_sub < cell.longest_path ? cell.longest_path : acc_sub
+      end
+    end
+  end
+
+  private
   def cell row, col
     if row >= 0 && row < hex.length && col >= 0 && col < hex[row].length
       hex[row][col]
@@ -50,31 +78,43 @@ class Hex
     end
   end
 
-  def solve
-    hex.each_with_index do |row, i|
-      row.each_with_index do |col , j|
-        join(i, j)
-      end
+  def neighbor_index row, col, pos
+    case pos
+    when :LEFT_ABOVE then
+      row -= 1
+      col -= 1 if row <= 3
+    when :RIGHT_AVOVE then
+      row -= 1
+      col += 1 if row > 3
+    when :LEFT then
+      col -= 1
+    when :RIGHT then
+      col += 1
+    when :LEFT_BENEATH then
+      row += 1
+      col -= 1 if row > 3
+    when :RIGHT_BENEATH then
+      row += 1
+      col += 1 if row <= 3
     end
+    [row, col]
   end
 
-  def join row, col
+  def notify row, col
     base = cell(row, col)
-    (tmp = cell(row-1, col-1)) && (base > tmp && tmp.recieve_signal(base.longest_path) && join(row-1, col-1))
-    (tmp = cell(row-1, col)) && (base > tmp && tmp.recieve_signal(base.longest_path) && join(row-1, col))
-    (tmp = cell(row, col-1)) && (base > tmp && tmp.recieve_signal(base.longest_path) && join(row, col-1))
-    (tmp = cell(row, col+1)) && (base > tmp && tmp.recieve_signal(base.longest_path) && join(row, col+1))
-    (tmp = cell(row+1, col)) && (base > tmp && tmp.recieve_signal(base.longest_path) && join(row+1, col))
-    (tmp = cell(row+1, col+1)) && (base > tmp && tmp.recieve_signal(base.longest_path) && join(row+1, col+1))
+    @@neighbor_pos.each do |pos|
+      n_row, n_col = neighbor_index(row, col, pos)
+      neighbor = cell(n_row, n_col)
+      if !neighbor.nil?
+        (base > neighbor) && neighbor.update_longest_path(base.longest_path) && notify(n_row, n_col)
+      end
+    end
   end
 end
 
 input = STDIN.gets.chomp
 hex = Hex.new(input)
-hex.show
-puts
+hex.search_longest_path
 hex.show_longest_path
 puts
-hex.solve
-puts
-hex.show_longest_path
+puts hex.longest_path
