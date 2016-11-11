1900f14bd445a51c62765083a44f4d5c415ad0a7
Fukayanegi <fukayanegi1981@gmail.com>
2016-11-12 00:40:13 +0900

    improve: 計算を畳み込み、メモリ消費を省力化することで高速化 Closes #230

--- question_of_this_week/scramble_cards_by_turns.rb
+++ question_of_this_week/scramble_cards_by_turns.rb
@@ -28,6 +28,16 @@ class Fixnum
   end
 end
 
+class Array
+  def histgram
+    self.inject({}) do |acc, value|
+      key = block_given? ? (yield value) : value
+      acc[key] = (acc[key] || 0) + 1
+      acc
+    end
+  end
+end
+
 class Solver
   attr_accessor :m, :n
 
@@ -50,41 +60,49 @@ class Solver
     answer
   end
 
+  def calc_duplication constitution, choices, rest
+    # dlog({:constitution => constitution, :choices => choices, :rest => rest})
+    if rest == 0
+      dlog({:constitution => constitution})
+      dlog({:choices => choices})
+      all_count = constitution.inject(0){|acc, (key, value)| acc += value}
+      dlog({:all_count => all_count})
+      first_count = choices.inject(0){|acc, (key, value)| acc += value}
+      first = first_count.permutation / choices.inject(1){|acc, (key, value)| acc * value.permutation}
+        
+      dlog({:first => first})
+      first_sum = choices.inject(0){|acc, (key, value)| acc + key * value}
+      second = (all_count - first_count).permutation / \
+        constitution.inject(1){|acc, (key, value)| acc * (value - (choices[key] || 0)).permutation}
+      dlog({:second => second})
+      second_sum = constitution.inject(0){|acc, (key, value)| acc + key * value} - first_sum
+      return first_sum > second_sum ? [first * second] : [0]
+    end
+
+    min_value = choices.keys.max || 0
+    return [0] if (constitution.select{|key, value| key > min_value}.values.inject(:+) || 0) < rest
+
+    dlog({:min_value => min_value})
+    answer = []
+    element, max_choice = constitution.sort_by{|key, value| key}.find{|key, value| key > min_value}
+    0.upto(max_choice < rest ? max_choice : rest).map do |choice|
+      choices[element] = choice
+      answer.concat(calc_duplication(constitution, choices, rest - choice))
+      choices.delete(element)
+    end
+    answer
+  end
+
   def solve
     # 奇数個の要素数で合計がmになる数値の組み合わせ
     patterns = (1..m).step(2).inject([]) do |acc, elements|
       acc.concat(create_cards_pattern(m, n, elements))
     end
 
-    # 要素数m/2(後手),m/2+1(先手)に分けたときにm/2+1の方が大きくなる組み合わせ
-    first_seconds = patterns.inject([]) do |acc, pattern|
-      dlog({:pattern => pattern})
+    answer = patterns.inject(0) do |acc, pattern|
       turns = pattern.length / 2 + 1
-      constitution = pattern.inject({}){|acc, v| acc[v] = (acc[v] || 0) + 1; acc}
-      first_second = pattern.combination(turns).select{|scramble| scramble.inject(:+) > m / 2}.uniq.map do |scramble|
-        tmp = scramble.inject(Hash.new(0)){|acc, v| acc[v] = (acc[v] || 0) + 1; acc}
-        rest = constitution.inject([]) do |acc, (key, value)|
-          (value - tmp[key]).times{acc << key}
-          acc
-        end
-        [scramble, rest]
-      end
-      acc.concat(first_second)
-    end
-    dlog({:first_seconds => first_seconds})
-    
-    # 先手、後手の組み合わせの取り方のパターン数を乗算
-    first_seconds.inject(0) do |acc, (first, second)|
-      dlog({:first => first, :second => second})
-      first_constitution = first.inject({}){|acc, v| acc[v] = (acc[v] || 0) + 1; acc}
-      dlog({:first_constitution => first_constitution})
-      first_patterns = first.length.factorial / first_constitution.inject(1){|acc_f, (key, value)| acc_f * value.permutation}
-
-      second_constitution = second.inject({}){|acc, v| acc[v] = (acc[v] || 0) + 1; acc}
-      dlog({:second_constitution => second_constitution})
-      second_patterns = second.length.factorial / second_constitution.inject(1){|acc_s, (key, value)| acc_s * value.permutation}
-      acc += first_patterns * second_patterns
-      acc
+      constitution = pattern.histgram
+      acc += calc_duplication(constitution, {}, turns).inject(:+)
     end
   end
 end
