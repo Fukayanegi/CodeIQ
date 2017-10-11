bc2fe14050954a327cda77ee82b4ab04447ccc2b
Fukayanegi <fukayanegi1981@gmail.com>
2017-10-11 14:32:17 +0900

    implement: 正解は導けるけど性能が悪し #275

--- skill_check/skill_check_S3.rb
+++ skill_check/skill_check_S3.rb
@@ -10,8 +10,6 @@ def dlog variables, method = ""
 end
 
 class Solver
-  @@Pos = Struct.new(:x, :y)
-
   def initialize size, height
     @size = size
     @height = height
@@ -21,6 +19,7 @@ class Solver
     end
 
     @board = generate_board(size, height)
+    # show_board
   end
 
   def generate_board size, height
@@ -49,45 +48,76 @@ class Solver
     others
   end
 
+  def show_board
+    @size.times do |y|
+      line = ""
+      @size.times do |x|
+        line << @board[x + y * @size].to_s
+      end
+      puts line
+    end
+  end
+
   def solve
     pos = 0
-    while pos < @size**2
-      height = @board[pos]
+    memo_lu = Array.new(@size * 2 - 1) {0}
+    memo_l = Array.new(@size * 2 - 1) {0}
+    answer = 1
 
-      # dlog({:call => "#{pos}, #{pos % @size}, #{pos / @size}, #{height}"}) if pos % 1000000 == 0
-      # solve_inner pos, height
+    while pos < @size * 2 - 1
+      # dlog({:pos => pos, :time => Time.now - START}) if pos % 100 == 0
+      adjust = (pos >= @size ? (pos - @size + 1) : 0)
+      loops = pos < @size ? pos + 1 : @size*2 - pos - 1
+      answer_tmp_prev = 0
 
-      left_length = @memo[height][pos - 1]
-      up_length = @memo[height][pos - @size]
-      left_up_length = @memo[height][pos - 1 - @size]
-      dlog({:pos => pos, :left_length => left_length, :up_length => up_length, :left_up_length => left_up_length}) if pos == 52814
+      loops.times do |i|
+        x = i + adjust
+        y = pos - i - adjust
+        height = @board[y * @size + x]
+        # dlog({:i => i, :x => x, :y => y, :height => height})
 
-      min = left_length < up_length ? left_length : up_length
-      min = min < left_up_length ? min : left_up_length
+        answer_tmp = 1
+        left = memo_l[i]
+        up = memo_l[i + 1]
+        if @board[y * @size + x - 1] == height && @board[(y - 1) * @size + x] == height && @board[(y - 1) * @size + x - 1] == height
+          # dlog({:memo_lu => memo_lu[i], :memo_l => memo_l, :left => left, :up => up})
+          answer_tmp = answer_tmp + [memo_lu[i], left, up].min
+        end
 
-      @memo[height][pos] = 1 + min
-  
-      pos += 1
-      # sleep 1
+        memo_lu[i] = pos <= @size - 2 ? left : up
+        memo_l[i] = pos <= @size - 2 ? answer_tmp_prev : answer_tmp
+        answer = answer_tmp if answer_tmp > answer
+
+        answer_tmp_prev = answer_tmp if pos <= @size - 2 
+      end
+
+      memo_l[loops] = answer_tmp_prev if pos <= @size - 2
+
+      # dlog({:memo_lu => memo_lu})
+      # dlog({:memo_l => memo_l})
+
+      pos = pos + 1
     end
-
-    answer = @memo.values.map{|h| h.values.max}.select{|v| v}.max
     answer**2
   end
-end
 
 # inputs = []
 # while line = STDIN.gets
 #   inputs << line.chomp.split(",").map(&:to_i)
 # end
-# inputs = [[13, 5]]
+# inputs = [[8, 5]]
 # inputs = [[8, 5], [10, 4], [13, 5]]
-# inputs = [[40,6], [100,5], [500,4], [1000,4], [1000,3], [2000,4], [2000,3], [3000,3]]
+# answers = [16, 16, 25]
+inputs = [[40,6], [100,5], [500,4], [1000,4], [1000,3], [2000,4], [2000,3], [3000,3]]
+# answers = [289, 2704, 8464, 48400, 48400, 281961, 254016, 456976]
 # inputs = [[40,6]]
-inputs = [[500,4]]
+# inputs = [[100,5]]
+# inputs = [[500,4]]
 # inputs = [[3000,3]]
 
+START = Time.now
 inputs.each do |(size, height)|
   solver = Solver.new(size, height)
   puts solver.solve
 end
+puts "#{Time.now - START}"
